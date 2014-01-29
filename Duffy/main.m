c2ebd65ff103853c07ef2ff5d0b61dcd280598bc
hbridge <hbridge@gmail.com>
2014-01-29 13:52:11 -0500

    Initial Commit

---
+++ Duffy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Duffy
+//
+//  Created by Henry Bridge on 1/29/14.
+//  Copyright (c) 2014 Duffy Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DFAppDelegate class]));
+    }
+}
