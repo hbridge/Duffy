a55e2d4ebb9f680b3426c942d9d0dd84690bdd14
hbridge <hbridge@gmail.com>
2014-03-24 17:35:58 -0400

    Removed stale create filename code.

--- Duffy/DFUploadController.m
+++ Duffy/DFUploadController.m
@@ -233,31 +233,6 @@ static DFUploadController *defaultUploadController;
     return _objectManager;
 }
 
-- (NSString *)uniqueFilenameForPhoto:(DFPhoto *)photo uploadDate:(NSDate *)date
-{
-    NSString *inputString = [NSString stringWithFormat:@"%@ %f", photo.alAssetURLString, [date timeIntervalSince1970]];
-    NSString *sha1 = [self sha1:inputString];
-    return sha1;
-}
-
--(NSString*) sha1:(NSString*)input
-{
-    const char *cstr = [input cStringUsingEncoding:NSUTF8StringEncoding];
-    NSData *data = [NSData dataWithBytes:cstr length:input.length];
-    
-    uint8_t digest[CC_SHA1_DIGEST_LENGTH];
-    
-    CC_SHA1(data.bytes, (int)data.length, digest);
-    
-    NSMutableString* output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
-    
-    for(int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++)
-        [output appendFormat:@"%02x", digest[i]];
-    
-    return output;
-}
-
-
 
 #pragma mark - Core Data helpers
 
