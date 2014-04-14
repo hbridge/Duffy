9c78410202c9fe4457dd5817bcbdbc0a959d7e2d
Aseem <asood123@yahoo.com>
2014-04-14 16:59:18 -0400

    switched over search view to have month headers

--- peanut/photos/viz_views.py
+++ peanut/photos/viz_views.py
@@ -13,7 +13,7 @@ from photos.models import Photo, User, Classification
 from photos import image_util, search_util
 from .forms import ManualAddPhoto
 
-from datetime import datetime
+from datetime import datetime, date
 from dateutil.relativedelta import relativedelta
 
 	
@@ -101,6 +101,12 @@ def search(request, user_id=None):
 		else:
 			imageSize = 78;
 
+		if data.has_key('groupThreshold'):
+			groupThreshold = int(data['groupThreshold'])
+		else:
+			groupThreshold = 1000;		
+
+
 		width = imageSize*2 #doubled  for retina
 
 		try:
@@ -121,12 +127,14 @@ def search(request, user_id=None):
 		searchResults = search_util.solrSearch(user.id, startDate, newQuery)
 
 		allResults = searchResults.count()
-		searchResults = searchResults[((page-1)*count):(count*page)]
+		#searchResults = searchResults[((page-1)*count):(count*page)]
+		photoResults = splitPhotosFromIndexbyMonth(request, user.id, searchResults, groupThreshold)
 
 		photoIdToThumb = dict()
 		for result in searchResults:
 			photoIdToThumb[result.photoId] = image_util.imageThumbnail(result.photoFilename, width, user.id)
 
+		'''
 		start = ((page-1)*count)+1
 		if (allResults > count*page):
 			end = count*page
@@ -139,18 +147,13 @@ def search(request, user_id=None):
 			previous = True
 		else:
 			previous = False
-
+		'''
 
 		context = {	'user' : user,
 					'imageSize': imageSize,
-					'start': start,
-					'end': end,
+					'photoResults': photoResults,
 					'resultSize': allResults,
-					'next': next,
-					'previous': previous,
-					'searchResults': searchResults,
 					'query': query,
-					'page': page,
 					'userId': userId,
 					'thumbnailBasepath': thumbnailBasepath,
 					'photoIdToThumb': photoIdToThumb}
@@ -183,7 +186,8 @@ def gallery(request, user_id):
 	else:
 		groupThreshold = 11
 
-	photos = getPhotosSplitByMonth(request, user.id, groupThreshold)
+	photoQuery = Photo.objects.filter(user_id=user.id)
+	photos = splitPhotosFromDBbyMonth(request, user.id, photoQuery, groupThreshold)
 
 	for entry in photos:
 		for photo in entry['mainPhotos']:
@@ -201,7 +205,6 @@ def gallery(request, user_id):
 
 
 def serveImage(request):
-
 	if (request.session['userid']):
 		userId = request.session['userid']
 	else:
@@ -226,30 +229,76 @@ def serveImage(request):
 
 # Helper functions
 
-def getPhotosSplitByMonth(request, userId, groupThreshold=None):
-	#photos = Photo.objects.filter(user_id = userId).order_by('time_taken')
+def splitPhotosFromDBbyMonth(request, userId, photoSet=None, groupThreshold=None):
+	if (photoSet == None):
+		photoSet = Photo.objects.filter(user_id=userId)
 
 	if (groupThreshold == None):
 		groupThreshold = 11
 
-	dates = Photo.objects.filter(user_id=userId).datetimes('time_taken', 'month')
+	dates = photoSet.datetimes('time_taken', 'month')
+	
 	photos = list()
 
 	entry = dict()
 	entry['date'] = 'Undated'
-	entry['mainPhotos'] = list(Photo.objects.filter(user_id=userId).filter(time_taken=None)[:groupThreshold])
-	entry['subPhotos'] = list(Photo.objects.filter(user_id=userId).filter(time_taken=None)[groupThreshold:])
+	entry['mainPhotos'] = list(photoSet.filter(time_taken=None)[:groupThreshold])
+	entry['subPhotos'] = list(photoSet.filter(time_taken=None)[groupThreshold:])
 	entry['count'] = len(entry['subPhotos'])
 	photos.append(entry)
 
 	for date in dates:
 		entry = dict()
 		entry['date'] = date.strftime('%b %Y')
-		entry['mainPhotos'] = list(Photo.objects.filter(user_id=userId).exclude(time_taken=None).exclude(time_taken__lt=date).exclude(time_taken__gt=date+relativedelta(months=1)).order_by('time_taken')[:groupThreshold])
-		entry['subPhotos'] = list(Photo.objects.filter(user_id=userId).exclude(time_taken=None).exclude(time_taken__lt=date).exclude(time_taken__gt=date+relativedelta(months=1)).order_by('time_taken')[groupThreshold:])
+		entry['mainPhotos'] = list(photoSet.exclude(time_taken=None).exclude(time_taken__lt=date).exclude(time_taken__gt=date+relativedelta(months=1)).order_by('time_taken')[:groupThreshold])
+		entry['subPhotos'] = list(photoSet.exclude(time_taken=None).exclude(time_taken__lt=date).exclude(time_taken__gt=date+relativedelta(months=1)).order_by('time_taken')[groupThreshold:])
 		entry['count'] = len(entry['subPhotos'])
 		photos.append(entry)
 
+	return photos
+
+def splitPhotosFromIndexbyMonth(request, userId, photoSet=None, groupThreshold=None):
+	if (photoSet == None):
+		photoSet = 	SearchQuerySet().filter(userId=userId)
+
+	if (groupThreshold == None):
+		groupThreshold = 1000
+
+	dateFacet = photoSet.date_facet('timeTaken', start_date=date(1900,1,1), end_date=date(2014,5,1), gap_by='month').facet('timeTaken', mincount=1, limit=-1, sort=False)
+	facetCounts = dateFacet.facet_counts()
+	
+	photos = list()
+
+	'''
+	entry = dict()
+	entry['date'] = 'Undated'
+	entry['mainPhotos'] = list(photoSet.filter(timeTaken=None)[:groupThreshold])
+	entry['subPhotos'] = list(photoSet.filter(timeTaken=None)[groupThreshold:])
+	entry['count'] = len(entry['subPhotos'])
+	photos.append(entry)
+	'''
+	print str(facetCounts['dates']['timeTaken'])
+	del facetCounts['dates']['timeTaken']['start']
+	del facetCounts['dates']['timeTaken']['end']
+	del facetCounts['dates']['timeTaken']['gap']
+	
+
+	print facetCounts
+
+	od = OrderedDict(sorted(facetCounts['dates']['timeTaken'].items()))
+
+	for dateKey, countVal in od.items():
+
+		print dateKey
+		print countVal
+		entry = dict()
+		startDate = datetime.strptime(dateKey[:-1], '%Y-%m-%dT%H:%M:%S')
+		entry['date'] = startDate.strftime('%b %Y')
+		newDate = startDate+relativedelta(months=1)
+		entry['photos'] = list(photoSet.exclude(timeTaken__lt=startDate).exclude(timeTaken__gt=newDate).order_by('timeTaken')[:groupThreshold])
+		entry['count'] = len(entry['photos'])
+		photos.append(entry)
+		
 	return photos
 
 def setSession(request, userId):
