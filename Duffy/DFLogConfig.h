b4f35fee4efd10228124fa36f7876c34de93cdef
hbridge <hbridge@gmail.com>
2014-04-23 18:19:21 -0400

    Fixed PCH issue with DFLogConfig

---
+++ Duffy/DFLogConfig.h
@@ -0,0 +1,11 @@
+//
+//  DFLogConfig.h
+//  Duffy
+//
+//  Created by Henry Bridge on 4/23/14.
+//  Copyright (c) 2014 Duffy Productions. All rights reserved.
+//
+
+#import "DDLog.h"
+
+extern int const ddLogLevel;
--- Duffy/LoggingConfig.h
+++
@@ -1,9 +0,0 @@
-#import "LoggingConfig.h"
-
-#import <CocoaLumberjack/DDLog.h>
-
-#ifdef DEBUG
-int const ddLogLevel = LOG_LEVEL_VERBOSE;
-#else
-int const ddLogLevel = LOG_LEVEL_WARN;
-#endif
