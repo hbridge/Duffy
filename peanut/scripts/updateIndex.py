55574a16116d9aee5b6eb7f2287a7ed93431caff
Aseem <asood123@yahoo.com>
2014-05-06 12:02:13 -0400

    changing indexing to look for changes in last 5 mins

--- peanut/scripts/updateIndex.py
+++ peanut/scripts/updateIndex.py
@@ -5,7 +5,7 @@ import time
 
 def main(argv):
 	while True:
-		dt = datetime.datetime.utcnow() - datetime.timedelta(seconds=60)
+		dt = datetime.datetime.utcnow() - datetime.timedelta(seconds=300)
 		timeStr = dt.strftime("%Y-%m-%dT%H:%M:%S")
 		subprocess.call("python /home/derek/prod/Duffy/peanut/manage.py update_index --start=" + timeStr, shell=True)
 		time.sleep(5)
