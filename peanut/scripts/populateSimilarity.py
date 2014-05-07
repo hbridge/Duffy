8be79e7bf5d8fe1862638dbb6bb23fea80246ccf
Aseem <asood123@yahoo.com>
2014-05-07 16:14:33 -0400

    adding clustering cron job

---
+++ peanut/scripts/populateSimilarity.py
@@ -0,0 +1,27 @@
+import sys, os
+import time
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "peanut.settings")
+
+parentPath = os.path.abspath("..")
+if parentPath not in sys.path:
+    sys.path.insert(0, parentPath)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "peanut.settings")
+
+from photos.models import Photo, User
+from peanut import settings
+from photos import cluster_util
+
+
+def main(argv):
+	while True:
+		allUsers = User.objects.all().filter(id__gt=75) #ignores first set of accounts
+		for user in allUsers:
+			photos = list(Photo.objects.all().filter(user_id=user.id).exclude(thumb_filename=None).filter(clustered_time=None).order_by('time_taken'))
+			cluster_util.addToClustersBulk(photos)
+		time.sleep(5)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
