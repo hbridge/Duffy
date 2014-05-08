5a2c6a43e6831bcaa1f2a33c0389987932a85abc
Aseem <asood123@yahoo.com>
2014-05-07 22:13:28 -0400

    log messages for populateSimilarity

--- peanut/scripts/populateSimilarity.py
+++ peanut/scripts/populateSimilarity.py
@@ -17,8 +17,8 @@ def main(argv):
 		allUsers = User.objects.all().filter(id__gt=75) #ignores first set of accounts
 		for user in allUsers:
 			photos = list(Photo.objects.all().filter(user_id=user.id).exclude(thumb_filename=None).filter(clustered_time=None).order_by('time_taken'))
-			print "userId: {0} |  photos: {1}".format(user.id, len(photos))
-			print "{0}: {1} rows".format(user.id, cluster_util.addToClustersBulk(photos))
+			print "userId: {0} |  Unclustered photos: {1}".format(user.id, len(photos))
+			print "{1} rows added".format(cluster_util.addToClustersBulk(photos))
 		time.sleep(5)
 
 if __name__ == "__main__":
