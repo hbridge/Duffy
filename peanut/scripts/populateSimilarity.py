dd772e8987299f9d9d6e23f22e5bdb888415893e
Derek Parham <dparham@gmail.com>
2014-05-09 16:25:20 -0400

    peanut:  update logging for scripts

--- peanut/scripts/populateSimilarity.py
+++ peanut/scripts/populateSimilarity.py
@@ -1,5 +1,6 @@
 import sys, os
 import time, datetime
+import logging
 
 parentPath = os.path.abspath("..")
 if parentPath not in sys.path:
@@ -13,23 +14,29 @@ from peanut import settings
 from photos import cluster_util
 
 def main(argv):
-	print "Starting... " + time.strftime("%c")
+	logger = logging.getLogger(__name__)
+	
+	logger.info("Starting... ")
 	while True:
 		results = Photo.objects.all().filter(user__gt=75).exclude(thumb_filename=None).filter(clustered_time=None).values('user').annotate(Count('user'))
 
 		if len(results) == 0:
-			print "Found no users with photos needing similarity processing at " + time.strftime("%c")
+			logger.info("Found no users with photos needing similarity processing at " + time.strftime("%c"))
 		for result in results:
 			userId = result['user']
-			print "Processing user id:  " + str(userId)
+			logger.info("Processing user id:  " + str(userId))
 			nonClusteredPhotos = list(Photo.objects.select_related().filter(user_id=userId).exclude(thumb_filename=None).filter(clustered_time=None).order_by('time_taken'))
 			
 			tStart = datetime.datetime.utcnow()
-			print("{0}: Unclustered photos: {1}".format(tStart, len(nonClusteredPhotos)))
+			logger.info("{0}: Unclustered photos: {1}".format(tStart, len(nonClusteredPhotos)))
 			count = cluster_util.addToClustersBulk(nonClusteredPhotos)
-			print("{0}: {1} rows added".format(datetime.datetime.utcnow()-tStart, count))
+			logger.info("{0}: {1} rows added".format(datetime.datetime.utcnow()-tStart, count))
 			
 		time.sleep(5)
 
 if __name__ == "__main__":
+	logging.basicConfig(filename='/var/log/duffy/similarity.log',
+						level=logging.DEBUG,
+						format='%(asctime)s %(levelname)s %(message)s')
+	logging.getLogger('django.db.backends').setLevel(logging.ERROR) 
 	main(sys.argv[1:])
