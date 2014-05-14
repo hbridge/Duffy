a7f82d2a6658d007210d9561083243dae442ae5d
Derek Parham <dparham@gmail.com>
2014-05-14 12:13:44 -0400

    get similarity running again

--- peanut/scripts/populateSimilarity.py
+++ peanut/scripts/populateSimilarity.py
@@ -18,21 +18,21 @@ def main(argv):
 	logger = logging.getLogger(__name__)
 	
 	logger.info("Starting... ")
-	#while True:
-	results = Photo.objects.all().filter(user__gt=75).exclude(thumb_filename=None).filter(clustered_time=None).values('user').annotate(Count('user'))
+	while True:
+		results = Photo.objects.all().filter(user_id__gt=75).exclude(thumb_filename=None).filter(clustered_time=None).values('user').annotate(Count('user'))
 
-	if len(results) > 0:	
-		for result in results:
-			userId = result['user']
-			logger.info("Processing user id:  " + str(userId))
-			nonClusteredPhotos = list(Photo.objects.select_related().filter(user_id=userId).exclude(thumb_filename=None).filter(clustered_time=None).order_by('time_taken'))
-			
-			tStart = datetime.datetime.utcnow()
-			logger.info("{0}: Unclustered photos: {1}".format(tStart, len(nonClusteredPhotos)))
-			count = cluster_util.addToClustersBulk(nonClusteredPhotos)
-			logger.info("{0}: {1} rows added".format(datetime.datetime.utcnow()-tStart, count))
-		#else:
-		#	time.sleep(1)	
+		if len(results) > 0:	
+			for result in results:
+				userId = result['user']
+				logger.info("Processing user id:  " + str(userId))
+				nonClusteredPhotos = list(Photo.objects.select_related().filter(user_id=userId).exclude(thumb_filename=None).filter(clustered_time=None).order_by('time_taken'))
+				
+				tStart = datetime.datetime.utcnow()
+				logger.info("{0}: Unclustered photos: {1}".format(tStart, len(nonClusteredPhotos)))
+				count = cluster_util.addToClustersBulk(nonClusteredPhotos)
+				logger.info("{0}: {1} rows added".format(datetime.datetime.utcnow()-tStart, count))
+		else:
+			time.sleep(1)	
 
 if __name__ == "__main__":
 	logging.basicConfig(filename='/var/log/duffy/similarity.log',
