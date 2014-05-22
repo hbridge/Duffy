2d5748b63cc6a7ee9d61644f9a5eba180c0fb314
Derek Parham <dparham@gmail.com>
2014-05-21 21:32:14 -0400

    update boolean logic so we don't go into an infinite loop

--- peanut/photos/viz_views.py
+++ peanut/photos/viz_views.py
@@ -94,7 +94,7 @@ def search(request):
 			startDate = allResults[0].timeTaken
 		(pageStartDate, pageEndDate) = search_util.pageToDates(page, startDate)
 		searchResults = search_util.solrSearch(user.id, pageStartDate, newQuery, pageEndDate)
-		while (searchResults.count() < 25 or pageEndDate > datetime.utcnow()):
+		while (searchResults.count() < 25 and pageEndDate < datetime.utcnow()):
 			pageEndDate = pageEndDate+relativedelta(months=3)
 			page +=1
 			searchResults = search_util.solrSearch(user.id, pageStartDate, newQuery, pageEndDate)
