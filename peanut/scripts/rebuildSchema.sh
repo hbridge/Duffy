2a6da245be60e861f67277524809a4253f913d1a
Derek Parham <dparham@gmail.com>
2014-05-27 15:45:59 -0400

    update schema script to do single letter autocomplete

--- peanut/scripts/rebuildSchema.sh
+++ peanut/scripts/rebuildSchema.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 python manage.py build_solr_schema | sed 5d > ~/apache-solr-3.5.0/example/solr/conf/schema.xml
+
+sed -i 's/minGramSize="2"/minGramSize="1"/' ~/apache-solr-3.5.0/example/solr/conf/schema.xml
+
+sudo restart duffy-solr
