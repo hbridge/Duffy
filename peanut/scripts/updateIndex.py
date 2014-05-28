19e3f8b5603cbed9d42dc80d58c79032451b9be5
Derek Parham <dparham@gmail.com>
2014-05-28 13:33:44 -0400

    speed up indexing

--- peanut/scripts/updateIndex.py
+++ peanut/scripts/updateIndex.py
@@ -5,10 +5,10 @@ import time
 
 def main(argv):
 	while True:
-		dt = datetime.datetime.utcnow() - datetime.timedelta(seconds=300)
+		dt = datetime.datetime.utcnow() - datetime.timedelta(seconds=10)
 		timeStr = dt.strftime("%Y-%m-%dT%H:%M:%S")
 		subprocess.call("python /home/derek/prod/Duffy/peanut/manage.py update_index --start=" + timeStr, shell=True)
-		time.sleep(5)
+		time.sleep(1)
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
