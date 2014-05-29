13ec223d99c8fd0f15258fa1bf2a864502093fd6
Derek Parham <dparham@gmail.com>
2014-05-29 19:17:20 -0400

    update torchScript

---
+++ peanut/scripts/dummyScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while :
+do
+	echo `date` $$ >> blah.log
+	echo `date` $$
+	sleep 3
+done
--- peanut/scripts/torchServer.py
+++ peanut/scripts/torchServer.py
@@ -3,45 +3,60 @@
 
 import json
 import subprocess, os, signal
+import logging
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 # Running on titanblack
 cmd = "torch main.lua"
-logfileName = "/home/duffy/torchLog.log"
+mainLogfilename = "/home/duffy/torchMainLog.log"
+torchLogfileName = "/home/duffy/torchLog.log"
+restartLogfileName =  "/home/duffy/torchRestartLog.log"
 
 # Running on duffy
 #cmd = "./dummyScript.sh"
-#logfileName = "/home/derek/torchLog.log"
+#mainLogfilename = "/home/derek/mainLog.log"
+#torchLogfileName = "/home/derek/torchLog.log"
+#restartLogfileName =  "/home/derek/torchRestartLog.log"
+
 
 class torchRunner():
 	def startTorch(self):
+		logging.debug("in startTorch")
 		response = []
-		self.proc = subprocess.Popen(cmd.split(), stdout=open(logfileName, 'a') )
+		self.proc = subprocess.Popen(cmd.split(), stdout=open(torchLogfileName, 'a') )
 		response.append("Started " + str(self.proc.pid))
 		return response
 
 	def stopTorch(self):
+		logging.debug("in stopTorch")
 		response = []
 		response.append("Trying to kill...")
 		if (self.proc):
 			response.append("Killing process " + str(self.proc.pid))
-			self.proc.terminate()
+			os.kill(self.proc.pid, signal.SIGHUP)
+			os.kill(self.proc.pid, signal.SIGINT)
+			os.kill(self.proc.pid, signal.SIGKILL)
 		return response
 
 	def restartTorch(self):
+		logging.debug("in restartTorch")
 		response = []
 		response.append(self.stopTorch())
 		response.append(self.startTorch())
 		return response
+
+	def getPid(self):
+		return self.proc.pid
 
 tr = torchRunner()
 	
 class HttpHandler(BaseHTTPRequestHandler):
 	def respondSuccess(self, response):
 		self.send_response(200)
-		self.send_header('Content-type',	'text/html')
+		self.send_header('Content-type', 'text/html')
 		self.end_headers()
 		self.wfile.write(json.dumps(response))
+		logging.debug("Just sent: " + json.dumps(response))
 		return
 
 	def do_GET(self):
@@ -50,10 +65,21 @@ class HttpHandler(BaseHTTPRequestHandler):
 
 			if self.path.startswith("/restart"):
 				ret = tr.restartTorch()
-				response['restarted'] = True
 				response['debug'] = ret
 				self.respondSuccess(response)
 				return
+			elif self.path.startswith("/stop"):
+				ret = tr.stopTorch()
+				response['debug'] = ret
+				self.respondSuccess(response)
+			elif self.path.startswith("/start"):
+				ret = tr.startTorch()
+				response['debug'] = ret
+				self.respondSuccess(response)
+			elif self.path.startswith("/getpid"):
+				ret = tr.getPid()
+				response['debug'] = str(ret)
+				self.respondSuccess(response)
 			else:
 				response['path'] = self.path
 				self.respondSuccess(response)
@@ -61,16 +87,19 @@ class HttpHandler(BaseHTTPRequestHandler):
 			return
 				
 		except IOError:
+			logging.error('File Not Found: %s' % self.path)
 			self.send_error(404,'File Not Found: %s' % self.path)
 
 def main():
 	try:
+		logging.basicConfig(filename=mainLogfilename, level=logging.DEBUG)
+
 		server = HTTPServer(('', 12345), HttpHandler)
-		print 'started httpserver...'
+		logging.info('started httpserver...')
 		tr.startTorch()
 		server.serve_forever()
 	except KeyboardInterrupt:
-		print '^C received, shutting down server'
+		logging.info('^C received, shutting down server')
 		tr.stopTorch()
 		server.socket.close()
 
