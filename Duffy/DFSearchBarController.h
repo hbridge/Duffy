51b8ff784c84c141acc18ae8d4861bcb0a01a46e
hbridge <hbridge@gmail.com>
2014-05-29 11:16:47 -0400

    New class

---
+++ Duffy/DFSearchBarController.h
@@ -0,0 +1,13 @@
+//
+//  DFSearchBarController.h
+//  Duffy
+//
+//  Created by Henry Bridge on 5/29/14.
+//  Copyright (c) 2014 Duffy Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DFSearchBarController : NSObject
+
+@end
---
+++ Duffy/DFSearchBarController.m
@@ -0,0 +1,13 @@
+//
+//  DFSearchBarController.m
+//  Duffy
+//
+//  Created by Henry Bridge on 5/29/14.
+//  Copyright (c) 2014 Duffy Productions. All rights reserved.
+//
+
+#import "DFSearchBarController.h"
+
+@implementation DFSearchBarController
+
+@end
