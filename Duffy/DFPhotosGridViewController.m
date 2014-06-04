bb6dcd19df2caa6360a2e77459c1722181b16afe
hbridge <hbridge@gmail.com>
2014-06-04 16:23:45 -0400

    3 photos per row instead of 4

--- Duffy/DFPhotosGridViewController.m
+++ Duffy/DFPhotosGridViewController.m
@@ -27,8 +27,8 @@
 @synthesize itemsBySection = _itemsBySection;
 @synthesize sectionNames = _sectionNames;
 
-static const CGFloat DEFAULT_PHOTO_SQUARE_SIZE = 78.5;
-static const CGFloat DEFAULT_PHOTO_SPACING = 2;
+static const CGFloat DEFAULT_PHOTO_SQUARE_SIZE = 105;
+static const CGFloat DEFAULT_PHOTO_SPACING = 2.5;
 
 - (id)init
 {
