33e72d14dadf4c689760f472481b1a27322cb7dc
Aseem <asood123@yahoo.com>
2014-06-10 19:31:04 -0400

    model changes for userbasesummary

--- peanut/arbus/viz_views.py
+++ peanut/arbus/viz_views.py
@@ -40,10 +40,10 @@ def userbaseSummary(request):
 						'3E8018C0-1BE5-483D-89C4-85CD66F81298',
 						'26A1609E-BBBA-4684-8DF0-A394500FA96B',
 						'BEEF'}
-	resultList = list()
+	arbusList = list()
 	to_zone = tz.gettz('America/New_York')
 
-	users = User.objects.all()
+	users = User.objects.filter(product_id=0)
 
 	for user in users:
 		entry = dict()
@@ -105,10 +105,64 @@ def userbaseSummary(request):
 						('ipad simulator'.lower() in user.first_name.lower())):
 						entry['internal'] = True
 						break
-		resultList.append(entry)
+		arbusList.append(entry)
 
 
-	context = {	'resultList': resultList}
+	users = User.objects.filter(product_id=1)
+	strandList = list()
+
+	for user in users:
+		entry = dict()
+		entry['user'] = user
+		if (user.added):
+			entry['userCreated'] = user.added.astimezone(to_zone).strftime('%Y/%m/%d %H:%M:%S')
+		dbQuery = Photo.objects.filter(user_id=user.id)
+		totalCount = dbQuery.count()
+		if (totalCount > 0):
+			photoSet = dbQuery.order_by('-added')[:1]
+			for photo in photoSet:
+				entry['lastUploadTime'] = photo.added.astimezone(to_zone).strftime('%Y/%m/%d %H:%M:%S')
+				break
+			photoSet = dbQuery.order_by('-updated')[:1]
+			for photo in photoSet:
+				entry['lastUpdatedTime'] = photo.updated.astimezone(to_zone).strftime('%Y/%m/%d %H:%M:%S')
+				break	
+
+			entry['dbCount'] = totalCount
+			entry['thumbsCount'] = dbQuery.exclude(thumb_filename=None).count()
+			entry['thumbs'] = int(math.floor(entry['thumbsCount']/totalCount*100))
+			photosWithGPS = dbQuery.filter((Q(metadata__contains='{GPS}') & Q(metadata__contains='Latitude')) | Q(location_point__isnull=False)).count()
+
+			if photosWithGPS > 0:
+				entry['twofish'] = int(math.floor(float(dbQuery.exclude(twofishes_data=None).count())/float(photosWithGPS)*100))
+			else:
+				entry['twofish'] = '-'
+			fullimagesCount = dbQuery.exclude(full_filename=None).count()
+			entry['fullimagesCount'] = fullimagesCount
+			entry['fullimages'] = entry['fullimagesCount']*100/totalCount
+
+			searchResults = SearchQuerySet().all().filter(userId=user.id)
+			entry['resultsCount'] = searchResults.count()*100/totalCount
+			
+			entry['clusteredCount'] = dbQuery.exclude(clustered_time=None).count()
+			entry['clustered'] = entry['clusteredCount']*100/totalCount
+
+			entry['internal'] = False
+
+			if (user.added == None or len(user.first_name) == 0):
+				entry['internal'] = True
+			else:
+				for phoneid in knownPhoneIds:
+					if ((phoneid.lower() in user.phone_id.lower()) or 
+						('iphone simulator'.lower() in user.first_name.lower()) or
+						('ipad simulator'.lower() in user.first_name.lower())):
+						entry['internal'] = True
+						break
+		strandList.append(entry)
+
+
+	context = {	'arbusList': arbusList,
+				'strandList': strandList}
 	return render(request, 'admin/userbaseSummary.html', context)
 
 # Helper functions
