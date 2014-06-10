39dae086351cff6e68dfb343d204519c84cad7b5
hbridge <hbridge@gmail.com>
2014-06-10 11:43:15 -0400

    Make the background for photos black when going into preview mode again.

--- Duffy/DFPhotoView.m
+++ Duffy/DFPhotoView.m
@@ -29,13 +29,16 @@
 
 - (void)configureViews
 {
+  self.backgroundColor = [UIColor clearColor];
   self.scrollView = [[UIScrollView alloc] initWithFrame:self.frame];
   self.imageView = [[UIImageView alloc] initWithFrame:self.frame];
   [self.scrollView addSubview:self.imageView];
   self.imageView.image = _image;
   self.imageView.contentMode = UIViewContentModeScaleAspectFit;
+  self.imageView.backgroundColor = [UIColor clearColor];
   self.scrollView.delegate = self;
   self.scrollView.maximumZoomScale = 2.0;
+  self.scrollView.backgroundColor = [UIColor clearColor];
   
   [self addSubview:self.scrollView];
 }
