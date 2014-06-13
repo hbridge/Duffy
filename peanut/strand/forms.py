fe42a11a946b10fe58b95e5ed794b43e59be0292
Aseem <asood123@yahoo.com>
2014-06-13 15:43:58 -0400

    supporting different date format

--- peanut/strand/forms.py
+++ peanut/strand/forms.py
@@ -5,8 +5,8 @@ class GetJoinableStrandsForm(forms.Form):
 	user_id = forms.IntegerField(min_value=1, max_value=10000)
 	lat = forms.FloatField(min_value=-90, max_value=90)
 	lon = forms.FloatField(min_value=-180, max_value=180)
-	start_date_time = forms.DateTimeField(required=False)
+	start_date_time = forms.DateTimeField(required=False, input_formats=['%Y-%m-%dT%H:%M:%SZ', '%Y-%m-%d %H:%M:%S'])
 
 class GetNewPhotosForm(forms.Form):
 	user_id = forms.IntegerField(min_value=1, max_value=10000)
-	start_date_time = forms.DateTimeField()
+	start_date_time = forms.DateTimeField(input_formats=['%Y-%m-%dT%H:%M:%SZ', '%Y-%m-%d %H:%M:%S'])
