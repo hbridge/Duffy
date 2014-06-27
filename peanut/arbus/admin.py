c206f7ff570525fc1d0207d1803e1a82afd8bf93
Derek Parham <dparham@gmail.com>
2014-06-27 16:25:45 -0400

    update admin models

--- peanut/arbus/admin.py
+++ peanut/arbus/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from common.models import Photo, User, Classification, Similarity
+from common.models import Photo, User, Classification, Similarity, Neighbor, NotificationLog
 
 # Register your models here.
 admin.site.register(Photo)
 admin.site.register(User)
 admin.site.register(Classification)
 admin.site.register(Similarity)
+admin.site.register(Neighbor)
+admin.site.register(NotificationLog)
