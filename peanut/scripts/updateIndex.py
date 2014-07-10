0af203e0acdbbf4c83a2a128327269b756846f94
Derek Parham <dparham@gmail.com>
2014-07-10 16:58:29 -0400

    Fix updateIndex.py bug, it didn't have settings context set

--- peanut/scripts/updateIndex.py
+++ peanut/scripts/updateIndex.py
@@ -7,7 +7,7 @@ def main(argv):
 	while True:
 		dt = datetime.datetime.utcnow() - datetime.timedelta(seconds=60)
 		timeStr = dt.strftime("%Y-%m-%dT%H:%M:%S")
-		subprocess.call("python /home/derek/prod/Duffy/peanut/manage.py update_index --start=" + timeStr, shell=True)
+		subprocess.call("python /home/derek/prod/Duffy/peanut/manage.py update_index --settings=peanut.settings.dev --start=" + timeStr, shell=True)
 		time.sleep(1)
 
 if __name__ == "__main__":
