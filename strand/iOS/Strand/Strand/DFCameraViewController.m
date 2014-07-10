5c0e68b816b4d86e868d94f492bc9a4c55d85171
hbridge <hbridge@gmail.com>
2014-07-10 16:00:05 -0400

    check to see if loc is good before updating server UI

--- strand/iOS/Strand/Strand/DFCameraViewController.m
+++ strand/iOS/Strand/Strand/DFCameraViewController.m
@@ -543,7 +543,9 @@ didFinishPickingMediaWithInfo:(NSDictionary *)info {
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
 {
   CLLocation *location = locations.lastObject;
-  [self updateServerUI];
+  if ([self isGoodLocation:location]) {
+    [self updateServerUI];
+  }
   
   DDLogInfo(@"DFCameraViewController updated location: <%f, %f> +/- %.02fm @ %@",
             location.coordinate.latitude,
