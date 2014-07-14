6a3e0e455037c7c9d9741223cdecb687023c2d42
Aseem <asood123@yahoo.com>
2014-07-14 14:05:08 -0400

    adding push notifications for enterprise build

--- peanut/strand/api_views.py
+++ peanut/strand/api_views.py
@@ -407,14 +407,14 @@ def register_apns_token(request):
 		# TODO (Aseem): Make this more efficient. Assume nothing!
 		user.device_token = deviceToken
 		apnsDev = APNService.objects.get(id=constants.IOS_NOTIFICATIONS_DEV_APNS_ID)
-		apnsDerekDev = APNService.objects.get(id=constants.IOS_NOTIFICATIONS_DEREK_DEV_APNS_ID)
 		apnsProd = APNService.objects.get(id=constants.IOS_NOTIFICATIONS_PROD_APNS_ID)
+		apnsEnterpriseProd = APNService.objects.get(id=IOS_NOTIFICATIONS_ENTERPRISE_PROD_APNS_ID)
 
 		devices = Device.objects.filter(token=deviceToken)
 
 		if (len(devices) == 0):
 			Device.objects.create(token=deviceToken, is_active=True, service=apnsDev)
-			Device.objects.create(token=deviceToken, is_active=True, service=apnsDerekDev)
+			Device.objects.create(token=deviceToken, is_active=True, service=apnsEnterpriseProd)
 			Device.objects.create(token=deviceToken, is_active=True, service=apnsProd)
 		else:
 			for device in devices:
