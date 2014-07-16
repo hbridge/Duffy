e69fe69bc4a7d1cb5649f9b5501755857bc81ae4
Derek Parham <dparham@gmail.com>
2014-07-16 12:18:31 -0400

    update prod push script to point to dns entry

--- peanut/scripts/prodPush.sh
+++ peanut/scripts/prodPush.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh -i ~derek/derek-key-pair-east.pem ubuntu@ec2-54-88-151-106.compute-1.amazonaws.com "cd Duffy/peanut; git fetch; git rebase origin/master"
-ssh -i ~derek/derek-key-pair-east.pem ubuntu@ec2-54-88-151-106.compute-1.amazonaws.com "sudo apachectl -k restart"
+ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "cd Duffy/peanut; git fetch; git rebase origin/master"
+ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "sudo apachectl -k restart"
 
 python /home/derek/prod/Duffy/peanut/tests/black_box_api_tests.py prod.strand.duffyapp.com 12
