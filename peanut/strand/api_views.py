3a19e7bfdabbd1d7158532ab3a0e1c25d408f87f
Derek Parham <dparham@gmail.com>
2014-07-21 19:55:55 -0400

    add back in accuracy logging

--- peanut/strand/api_views.py
+++ peanut/strand/api_views.py
@@ -423,6 +423,7 @@ def update_user_location(request):
 		lon = form.cleaned_data['lon']
 		lat = form.cleaned_data['lat']
 		timestamp = form.cleaned_data['timestamp']
+		accuracy = form.cleaned_data['accuracy']
 
 		if (not timestamp):
 			timestamp = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
@@ -439,8 +440,7 @@ def update_user_location(request):
 				user.last_location_point = fromstr("POINT(%s %s)" % (lon, lat))
 				user.last_location_timestamp = timestamp
 				user.save()
-				logger.info("Location updated for user %s. %s: %s, %s" % (userId, datetime.datetime.utcnow().replace(tzinfo=pytz.utc), userId, user.last_location_point))
-			else:
+				logger.info("Location updated for user %s. %s: %s, %s, %s" % (userId, datetime.datetime.utcnow().replace(tzinfo=pytz.utc), userId, user.last_location_point, accuracy))			else:
 				logger.info("Location NOT updated for user %s. Old Timestamp. %s: %s, %s" % (userId, timestamp, userId, str((lon, lat))))
 		else:
 			logger.info("Location NOT updated for user %s. Lat/Lon Zero. %s: %s, %s" % (userId, datetime.datetime.utcnow().replace(tzinfo=pytz.utc), userId, str((lon, lat))))
