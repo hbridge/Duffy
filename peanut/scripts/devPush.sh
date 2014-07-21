55b3843bf6f80d313bff0a011e8c6c712cca904e
Derek Parham <dparham@gmail.com>
2014-07-21 19:13:24 -0400

    restart scripts on pushes

--- peanut/scripts/devPush.sh
+++ peanut/scripts/devPush.sh
@@ -7,10 +7,6 @@ sudo apachectl -k restart
 
 python /home/derek/prod/Duffy/peanut/tests/black_box_api_tests.py dev.duffyapp.com 297
 
-read -p "Restart scripts? (y/n) " RESP
-if [ "$RESP" = "y" ]; then
-  sudo restart duffy-strand-notifications
-  sudo restart duffy-neighbor
-else
-  echo "Not restarting scripts"
-fi
+echo "Restarting scripts..."
+sudo restart duffy-strand-notifications
+sudo restart duffy-neighbor
--- peanut/scripts/prodPush.sh
+++ peanut/scripts/prodPush.sh
@@ -5,11 +5,7 @@ ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "sudo apac
 
 python /home/derek/prod/Duffy/peanut/tests/black_box_api_tests.py prod.strand.duffyapp.com 12
 
-read -p "Restart scripts? (y/n) " RESP
-if [ "$RESP" = "y" ]; then
-  ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "sudo restart duffy-strand-notifications"
-  ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "sudo restart duffy-neighbor" 
-else
-  echo "Not restarting scripts"
-fi
+echo "Restarting scripts..."
+ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "sudo restart duffy-strand-notifications"
+ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "sudo restart duffy-neighbor" 
 
