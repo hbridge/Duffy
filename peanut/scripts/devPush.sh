ea932c08217750400f6087da379f5ba6e24b2234
Derek Parham <dparham@gmail.com>
2014-07-21 18:41:02 -0400

    update push scripts to ask if to restart scripts

--- peanut/scripts/devPush.sh
+++ peanut/scripts/devPush.sh
@@ -6,3 +6,11 @@ git rebase origin/master
 sudo apachectl -k restart
 
 python /home/derek/prod/Duffy/peanut/tests/black_box_api_tests.py dev.duffyapp.com 297
+
+read -p "Restart scripts? (y/n) " RESP
+if [ "$RESP" = "y" ]; then
+  sudo restart duffy-strand-notifications
+  sudo restart duffy-neighbor
+else
+  echo "Not restarting scripts"
+fi
--- peanut/scripts/prodPush.sh
+++ peanut/scripts/prodPush.sh
@@ -4,3 +4,12 @@ ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "cd Duffy/
 ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "sudo apachectl -k restart"
 
 python /home/derek/prod/Duffy/peanut/tests/black_box_api_tests.py prod.strand.duffyapp.com 12
+
+read -p "Restart scripts? (y/n) " RESP
+if [ "$RESP" = "y" ]; then
+  ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "sudo restart duffy-strand-notifications"
+  ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "sudo restart duffy-neighbor" 
+else
+  echo "Not restarting scripts"
+fi
+
