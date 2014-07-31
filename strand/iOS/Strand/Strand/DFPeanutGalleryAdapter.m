b69ec33dd78b464cc2326a912696dc218ec898da
hbridge <hbridge@gmail.com>
2014-07-31 11:19:51 -0400

    Point dev to the new feed

--- strand/iOS/Strand/Strand/DFPeanutGalleryAdapter.m
+++ strand/iOS/Strand/Strand/DFPeanutGalleryAdapter.m
@@ -11,7 +11,12 @@
 #import "DFPeanutSearchResponse.h"
 #import "DFDataHasher.h"
 
+#ifdef DEBUG
+NSString *const GalleryPath = @"strand_feed";
+#else
 NSString *const GalleryPath = @"neighbors";
+#endif
+
 
 @implementation DFPeanutGalleryAdapter 
 
