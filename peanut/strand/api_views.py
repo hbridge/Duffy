8bdea0cfc3a44bdf767f58664eb80ee03fd3c065
Derek Parham <dparham@gmail.com>
2014-08-04 16:04:09 -0400

    fix bug if they don't have a last location

--- peanut/strand/api_views.py
+++ peanut/strand/api_views.py
@@ -351,15 +351,16 @@ def strand_feed(request):
 		# now sort groups by the time_taken of the first photo in each group
 		groups = sorted(groups, key=lambda x: x[0].time_taken, reverse=True)
 
-		joinableStrands = getNearbyStrands(userId, user.last_location_point.x, user.last_location_point.y)
+		if user.last_location:
+			joinableStrands = getNearbyStrands(userId, user.last_location_point.x, user.last_location_point.y)
 
-		# Lastly, grab all our locked strands and add in those photos
-		lockedGroup = list()
-		for strand in joinableStrands:
-			lockedGroup.extend(strand.photos.all())
+			# Lastly, grab all our locked strands and add in those photos
+			lockedGroup = list()
+			for strand in joinableStrands:
+				lockedGroup.extend(strand.photos.all())
 
-		if len(lockedGroup) > 0:
-			groups.insert(0, lockedGroup)
+			if len(lockedGroup) > 0:
+				groups.insert(0, lockedGroup)
 
 		# Now we have to turn into our Duffy JSON, first, convert into the right format
 		formattedGroups = getFormattedGroups(groups, userId)
