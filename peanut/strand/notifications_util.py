0f27c08f784e3341076fa38cbd7efc8a3f9a70d5
Derek Parham <dparham@gmail.com>
2014-08-06 16:59:28 -0400

    try fetching minimum number of elements2

--- peanut/strand/notifications_util.py
+++ peanut/strand/notifications_util.py
@@ -119,5 +119,5 @@ def getNotificationLogs(timeWithinCutoff):
 """
 def getNotificationLogsForType(timeWithinCutoff, msgType):
 	# Grap notification logs from last hour.  If a user isn't in here, then they weren't notified
-	notificationLogs = NotificationLog.objects.filter(msg_type=msgType, filter(added__gt=timeWithinCutoff)
+	notificationLogs = NotificationLog.objects.filter(msg_type=msgType).filter(added__gt=timeWithinCutoff).filter(apns=1)
 	return notificationLogs
