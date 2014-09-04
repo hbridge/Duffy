37a04fcd9c6e033f6abfdabcc2bb34336e942f06
Derek Parham <dparham@gmail.com>
2014-09-04 16:53:53 -0400

    change up push scripts to look for script crashes

--- peanut/scripts/devPush.sh
+++ peanut/scripts/devPush.sh
@@ -5,8 +5,6 @@ git fetch
 git rebase origin/master
 sudo apachectl -k restart
 
-python /home/derek/prod/Duffy/peanut/tests/black_box_api_tests.py dev.duffyapp.com 297
-
 echo "Restarting scripts..."
 sudo stop duffy-strand-notifications
 sudo start duffy-strand-notifications
@@ -14,3 +12,9 @@ sudo stop duffy-friends
 sudo start duffy-friends
 sudo stop duffy-stranding
 sudo start duffy-stranding
+
+python /home/derek/prod/Duffy/peanut/tests/black_box_api_tests.py dev.duffyapp.com 297
+
+initctl list | grep duffy-strand-notifications
+initctl list | grep duffy-friends
+initctl list | grep duffy-stranding
