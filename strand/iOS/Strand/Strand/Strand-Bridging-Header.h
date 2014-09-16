5bb7a4c3bee7868dd696ed6c59d4f63d50fd592a
hbridge <hbridge@gmail.com>
2014-09-16 19:05:37 -0400

    Fix compiler warnings

--- strand/iOS/Strand/Strand/Strand-Bridging-Header.h
+++ strand/iOS/Strand/Strand/Strand-Bridging-Header.h
@@ -2,9 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <CocoaLumberjack/DDLog.h>
-#import "DFLogConfig.h"
-#import "DFTypedefs.h"
 #import "MMPopLabel.h"
 
 
