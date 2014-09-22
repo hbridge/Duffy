c955a327a08f37116268e3e9aa5e92a0b9144360
Derek Parham <dparham@gmail.com>
2014-09-22 19:17:22 +0000

    remove strand-notifications

--- peanut/scripts/devPush.sh
+++ peanut/scripts/devPush.sh
@@ -6,8 +6,6 @@ git rebase origin/master
 sudo apachectl -k restart
 
 echo "Restarting scripts..."
-sudo stop duffy-strand-notifications
-sudo start duffy-strand-notifications
 sudo stop duffy-friends
 sudo start duffy-friends
 sudo stop duffy-stranding
