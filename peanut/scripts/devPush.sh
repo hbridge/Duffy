8440b1a122136bbbc86e86f265f5f87a78634709
Derek Parham <dparham@gmail.com>
2014-09-22 18:23:33 +0000

    fix up tests to use virtualenv

--- peanut/scripts/devPush.sh
+++ peanut/scripts/devPush.sh
@@ -13,7 +13,7 @@ sudo start duffy-friends
 sudo stop duffy-stranding
 sudo start duffy-stranding
 
-python /home/ubuntu/dev/Duffy/peanut/tests/black_box_api_tests.py dev.duffyapp.com 297
+source /home/ubuntu/env/bin/activate; python /home/ubuntu/dev/Duffy/peanut/tests/black_box_api_tests.py dev.duffyapp.com 297
 
 initctl list | grep duffy-strand-notifications
 initctl list | grep duffy-friends
