fbf873989719348e6486813d127f50edeb9ea942
Derek Parham <dparham@gmail.com>
2014-09-25 00:29:40 +0000

    update tests for prod

--- peanut/scripts/prodPush.sh
+++ peanut/scripts/prodPush.sh
@@ -3,7 +3,7 @@
 ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "cd Duffy/peanut; git fetch; git rebase origin/master"
 ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "sudo apachectl -k restart"
 
-python /home/derek/prod/Duffy/peanut/tests/black_box_api_tests.py prod.strand.duffyapp.com 12
+python /home/ubuntu/Duffy/peanut/tests/black_box_api_tests.py prod.strand.duffyapp.com 12
 
 echo "Restarting scripts..."
 ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "sudo restart duffy-strand-notifications"
