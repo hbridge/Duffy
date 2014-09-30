7203a7e8e196e0fa1bb42cca42a9b96a83ba847e
Derek Parham <dparham@gmail.com>
2014-09-30 17:03:34 +0000

    remove strand notifications

--- peanut/scripts/prodPush.sh
+++ peanut/scripts/prodPush.sh
@@ -6,7 +6,6 @@ ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "sudo apac
 python /home/ubuntu/dev/Duffy/peanut/tests/black_box_api_tests.py prod.strand.duffyapp.com 5010
 
 echo "Restarting scripts..."
-ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "sudo restart duffy-strand-notifications"
 ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "sudo restart duffy-friends"
 ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "sudo restart duffy-stranding" 
 
