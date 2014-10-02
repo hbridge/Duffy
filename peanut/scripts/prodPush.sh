883d7aaaff544894c76eaaf3ef013f6b244fa012
asood123 <asood123@yahoo.com>
2014-10-02 15:16:33 +0000

    updating account for testing on prod

--- peanut/scripts/prodPush.sh
+++ peanut/scripts/prodPush.sh
@@ -3,7 +3,7 @@
 ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "cd Duffy/peanut; git fetch; git rebase origin/master"
 ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "sudo apachectl -k restart"
 
-python /home/ubuntu/dev/Duffy/peanut/tests/black_box_api_tests.py prod.strand.duffyapp.com 5010
+python /home/ubuntu/dev/Duffy/peanut/tests/black_box_api_tests.py prod.strand.duffyapp.com 5020
 
 echo "Restarting scripts..."
 ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "sudo restart duffy-friends"
