9d491cccabbe8aaf54628530248977a4aee50815
hbridge <hbridge@gmail.com>
2014-10-03 17:43:55 -0400

    Add a gray border around create table view cells

---
+++ strand/iOS/Strand/Strand/DFOutlineSubviewsView.h
@@ -0,0 +1,17 @@
+//
+//  DFOutlineSubviewsView.h
+//  Strand
+//
+//  Created by Henry Bridge on 10/3/14.
+//  Copyright (c) 2014 Duffy Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DFOutlineSubviewsView : UIView
+
+@property (nonatomic, retain) UIColor *outlineColor;
+@property (nonatomic) CGFloat outlineThickness;
+@property (nonatomic) CGFloat outlineCornerRadius;
+
+@end
---
+++ strand/iOS/Strand/Strand/DFOutlineSubviewsView.m
@@ -0,0 +1,32 @@
+//
+//  DFOutlineSubviewsView.m
+//  Strand
+//
+//  Created by Henry Bridge on 10/3/14.
+//  Copyright (c) 2014 Duffy Inc. All rights reserved.
+//
+
+#import "DFOutlineSubviewsView.h"
+
+@implementation DFOutlineSubviewsView
+
+
+- (void)drawRect:(CGRect)rect {
+  [super drawRect:rect];
+  
+  for (UIView *subview in self.subviews) {
+    CGRect solidFrame = subview.frame;
+    // setup
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    CGContextSetLineWidth(ctx, self.outlineThickness);
+    CGContextSetStrokeColorWithColor(ctx, [self.outlineColor CGColor]);
+    
+    // rounded rect at border
+    UIBezierPath *roundedRect = [UIBezierPath bezierPathWithRoundedRect:solidFrame
+                                                           cornerRadius:self.outlineCornerRadius];
+    CGContextAddPath(ctx, roundedRect.CGPath);
+    CGContextStrokePath(ctx);
+  }
+}
+
+@end
