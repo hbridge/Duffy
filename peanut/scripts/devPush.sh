773da8f8831c565cf331dd9382f81c1782806839
Derek Parham <dparham@gmail.com>
2014-10-22 15:30:21 +0000

    strand notifications back into it2

--- peanut/scripts/devPush.sh
+++ peanut/scripts/devPush.sh
@@ -11,7 +11,7 @@ sudo start duffy-friends
 sudo stop duffy-stranding
 sudo start duffy-stranding
 sudo stop duffy-strand-notifications
-sudo start dduffy-strand-notifications
+sudo start duffy-strand-notifications
 
 source /home/ubuntu/env/bin/activate; python /home/ubuntu/dev/Duffy/peanut/tests/black_box_api_tests.py dev.duffyapp.com 297
 
