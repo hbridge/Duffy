05954dd6cc601096696116657b2502191121ebde
asood123 <asood123@yahoo.com>
2014-10-25 14:11:09 +0000

    adding more stats to dashboard

--- peanut/arbus/viz_views.py
+++ peanut/arbus/viz_views.py
@@ -25,56 +25,35 @@ from arbus.forms import ManualAddPhoto
 logger = logging.getLogger(__name__)
 	
 def userbaseSummary(request):
-	knownPhoneIds = {	'AA94D207-D1E8-4098-9A39-76A3A1CC81FB',
-						'BA8E652E-6BD4-4DC7-B8A0-7157FFA51DEC',
-						'0012F94E-E6AF-429A-8530-E1011E1BFCAE',
-						'CEE91F90-263A-4BF1-AED7-6AB27B7BC076',
-						'F7092B08-EF4D-40EF-896D-0539CB102D3D',
-						'3E8018C0-1BE5-483D-89C4-85CD66F81298',
-						'26A1609E-BBBA-4684-8DF0-A394500FA96B',
-						'1067880F-AB7B-4B06-95AB-DE5216DD3CA0',
-						'55BE5A34-0188-4B0B-805E-45B30808CA61',
-						'BEEF'}
-
-	arbusList = list()
-	to_zone = tz.gettz('America/New_York')
-
-	# StrandV2-related code
-	strandV2List = list()
 
 
+	# database calls to fetch the data needed for the big user table
 	userStats = User.objects.filter(product_id=2).annotate(totalCount=Count('photo'), thumbsCount=Count('photo__thumb_filename'), 
 			photosWithGPS=Count('photo__location_point'), twofishCount=Count('photo__twofishes_data'), 
 			fullImagesCount=Count('photo__full_filename'), clusteredCount=Count('photo__clustered_time'), 
 			strandedCount=Count('photo__strand_evaluated'), lastUpdated=Max('photo__updated'), lastAdded=Max('photo__added'))
 
-	# This photo call is taking over a second on the dev database right now.
-	#photoDataRaw = Photo.objects.filter(thumb_filename__isnull=False).exclude(added__lt=(datetime.now()-timedelta(hours=168))).values('user').annotate(weeklyPhotos=Count('user'))
-	strandDataRaw = Strand.objects.filter(private=False).exclude(added__lt=(datetime.now()-timedelta(hours=168))).values('users').annotate(weeklyStrands=Count('users'), weeklyPhotos=Count('photos'))
-
+	photoDataRaw = Photo.objects.filter(thumb_filename__isnull=False).values('user').annotate(weeklyPhotos=Count('user'))
+	strandDataRaw = Strand.objects.filter(private=False).exclude(added__lt=(datetime.now()-timedelta(hours=168))).values('users').annotate(weeklyStrands=Count('users'), weeklyPhotos=Count('photos__user'))
 	actionDataRaw = Action.objects.exclude(added__lt=(datetime.now()-timedelta(hours=168))).values('user', 'action_type').annotate(weeklyActions=Count('user'))
 	lastActionDateRaw = Action.objects.all().exclude(added__lt=(datetime.now()-timedelta(hours=168))).values('user', 'action_type').annotate(lastActionTimestamp=Max('added'))
 	#friendsDataRaw = FriendConnection.objects.exclude(added__lt=(datetime.now()-timedelta(hours=168))).values('user').order_by().annotate(totalFriends=Count('user'))
-	#contactsDataRaw = ContactEntry.objects.exclude(added__lt=(datetime.now()-timedelta(hours=168))).values('user').order_by().annotate(totalContacts=Count('user'))	
 
-	#actionsCount = list(User.objects.filter(product_id=2).annotate(totalActions=Count('action')).order_by('-id'))
-	#strandCount = list(User.objects.filter(product_id=1).annotate(totalStrands=Count('strand__shared')).order_by('-id'))
 	inviteCount = list(User.objects.filter(product_id=2).annotate(totalInvites=Count('inviting_user')).order_by('-id'))
 	contactCount = list(User.objects.filter(product_id=2).annotate(totalContacts=Count('contactentry')).order_by('-id'))
 	friendCount = list(User.objects.filter(product_id=2).annotate(totalFriends1=Count('friend_user_1', distinct=True), totalFriends2=Count('friend_user_2', distinct=True)).order_by('-id'))
 
-	extras = dict()
+	# Exclude type GPS fetch since it happens so frequently
+	notificationDataRaw = NotificationLog.objects.filter(result=constants.IOS_NOTIFICATIONS_RESULT_SENT).exclude(msg_type=constants.NOTIFICATIONS_FETCH_GPS_ID).exclude(msg_type=constants.NOTIFICATIONS_REFRESH_FEED).exclude(added__lt=(datetime.now()-timedelta(hours=168))).values('user').order_by().annotate(totalNotifs=Count('user'), lastSent=Max('added'))
+
+	extras = dict() #store additional information per user
 	for i in range(len(userStats)):
 		entry = dict()
-		#entry['actions'] = actionsCount[i].totalActions
-		#entry['strands'] = strandCount[i].totalStrands
 		entry['contacts'] = contactCount[i].totalContacts
 		entry['friends'] = friendCount[i].totalFriends1 + friendCount[i].totalFriends2
 		entry['invites'] = inviteCount[i].totalInvites
 		extras[contactCount[i].id] = entry
 
-	# Exclude type GPS fetch since it happens so frequently
-	notificationDataRaw = NotificationLog.objects.filter(result=constants.IOS_NOTIFICATIONS_RESULT_SENT).exclude(msg_type=constants.NOTIFICATIONS_FETCH_GPS_ID).exclude(msg_type=constants.NOTIFICATIONS_REFRESH_FEED).exclude(added__lt=(datetime.now()-timedelta(hours=168))).values('user').order_by().annotate(totalNotifs=Count('user'), lastSent=Max('added'))
 	notificationCountById = dict()
 	notificationLastById = dict()
 
@@ -102,7 +81,6 @@ def userbaseSummary(request):
 	lastActionTimeById = dict()
 
 	for actionData in actionDataRaw:
-		#weeklyActionsById[actionData['user']] = actionData['weeklyActions']
 		if (actionData['action_type'] == 0):
 			weeklyFavsById[actionData['user']] = actionData['weeklyActions']
 		elif (actionData['action_type'] == 1):
@@ -118,6 +96,13 @@ def userbaseSummary(request):
 				lastActionTimeById[lastActionDate['user']] = lastActionDate['lastActionTimestamp']
 		else:
 			lastActionTimeById[lastActionDate['user']] = lastActionDate['lastActionTimestamp']
+
+	strandV2List = list()
+	to_zone = tz.gettz('America/New_York')
+	peopleCounts = dict()
+	peopleCounts['photosMetadata'] = 0
+	peopleCounts['photosShared'] = 0
+	peopleCounts['friends'] = 0
 
 	for i, user in enumerate(userStats):
 		entry = dict()
@@ -198,24 +183,16 @@ def userbaseSummary(request):
 		if ((len(user.display_name) == 0) or 
 			('555555' in str(user.phone_number))):
 			entry['internal'] = True
-		else:
-			for phoneid in knownPhoneIds:
-				if ((phoneid.lower() in user.phone_id.lower()) or 
-					('iphone simulator'.lower() in user.display_name.lower()) or
-					('ipad simulator'.lower() in user.display_name.lower())):
-					entry['internal'] = True
-					break
-			
-			for phoneNum in constants.DEV_PHONE_NUMBERS:
-				if (user.phone_number and phoneNum in str(user.phone_number)):
-					entry['internal'] = True
-					break
+
+		peopleCounts['photosMetadata'] += user.totalCount
+		peopleCounts['photosShared'] += user.thumbsCount
+		peopleCounts['friends'] += entry['friendCount']
 
 		strandV2List.append(entry)
 
 	strandV2List = sorted(strandV2List, key=lambda x: x['lastActionTimestamp'], reverse=True)
 
-	# stats on strands
+	# database calls for top tables
 	strands = Strand.objects.prefetch_related('photos', 'users').filter(product_id=2).filter(private=False)
 
 	strandBucket1 = strandBucket2 = strandBucket3 = strandBucket4 = 0
@@ -272,10 +249,14 @@ def userbaseSummary(request):
 
 	inviteCounts['swapped'] = Action.objects.select_related().filter(action_type=constants.ACTION_TYPE_ADD_PHOTOS_TO_STRAND).annotate(strandUsers=Count('strand__users')).filter(strandUsers__gt=1).count()
 
+	peopleCounts['friends'] = peopleCounts['friends']/2 #dividing by two to count relationships
+	peopleCounts['all'] = len(strandV2List)
+
 	context = {	'strandV2List': strandV2List,
 				'strandCounts': strandCounts,
 				'userCounts': userCounts,
-				'inviteCounts': inviteCounts}
+				'inviteCounts': inviteCounts,
+				'peopleCounts': peopleCounts}
 
 	return render(request, 'admin/userbaseSummary.html', context)
 
