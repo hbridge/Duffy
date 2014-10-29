808c246a9e6383a345a4aa4600ea4d5e2997b03c
hbridge <hbridge@gmail.com>
2014-10-29 18:36:45 -0400

    Remove binary string fn

--- strand/iOS/Strand/Strand/DFInboxViewController.m
+++ strand/iOS/Strand/Strand/DFInboxViewController.m
@@ -294,32 +294,6 @@
   return cell;
 }
 
-NSString * binaryStringFromInteger( NSInteger  number )
-{
-  NSMutableString * string = [[NSMutableString alloc] init];
-  
-  int spacing = pow( 2, 3 );
-  int width = ( sizeof( number ) ) * spacing;
-  int binaryDigit = 0;
-  NSInteger integer = number;
-  
-  while( binaryDigit < width )
-  {
-    binaryDigit++;
-    
-    [string insertString:( (integer & 1) ? @"1" : @"0" )atIndex:0];
-    
-    if( binaryDigit % spacing == 0 && binaryDigit != width )
-    {
-      [string insertString:@" " atIndex:0];
-    }
-    
-    integer = integer >> 1;
-  }
-  
-  return string;
-}
-
 + (void)resetCell:(DFInboxTableViewCell *)cell
 {
   cell.timeLabel.text = @"T";
