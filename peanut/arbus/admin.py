c80022fb615e3578e04b4f2e830c99aa7af25e7a
Derek Parham <dparham@gmail.com>
2014-11-05 17:35:02 +0000

    remove user admin page stuff

--- peanut/arbus/admin.py
+++ peanut/arbus/admin.py
@@ -12,8 +12,7 @@ class ActionAdmin(admin.ModelAdmin):
 admin.site.register(Action, ActionAdmin)
 
 class UserAdmin(admin.ModelAdmin):
-	readonly_fields = ['missingPhotos']
-	list_display = ['id', 'display_name', 'phone_number', 'photos_info', 'private_strands', 'shared_strands']
+	list_display = ['id', 'display_name', 'phone_number',]
 admin.site.register(User, UserAdmin)
 
 class PhotoAdmin(admin.ModelAdmin):
