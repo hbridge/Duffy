7bf609c2a07307efce199753aac1a66d9e8deebd
asood123 <asood123@yahoo.com>
2014-11-13 16:00:28 +0000

    added phone_number to notificationLog

--- peanut/common/models.py
+++ peanut/common/models.py
@@ -451,6 +451,7 @@ class Similarity(models.Model):
 
 class NotificationLog(models.Model):
 	user = models.ForeignKey(User)
+	phone_number = PhoneNumberField(null=True, db_index=True)
 	device_token = models.TextField(null=True)
 	msg = models.TextField(null=True)
 	msg_type = models.IntegerField(db_index=True)
