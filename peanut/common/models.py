6657fe0a71037f2f303e2357000bf568cdb4e989
Derek Parham <dparham@gmail.com>
2014-11-19 19:59:06 +0000

    put old field back in

--- peanut/common/models.py
+++ peanut/common/models.py
@@ -175,6 +175,7 @@ class Photo(models.Model):
 	neighbored_time = models.DateTimeField(null=True)
 	strand_evaluated = models.BooleanField(default=False, db_index=True)
 	strand_needs_reeval = models.BooleanField(default=False, db_index=True)
+	taken_with_strand = models.BooleanField(default=False)
 	saved_with_swap = models.BooleanField(default=False)
 	file_key = models.CharField(max_length=100, null=True)
 	bulk_batch_key = models.IntegerField(null=True, db_index=True)
