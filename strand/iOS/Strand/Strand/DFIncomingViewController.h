b7dd732927902b8e63e47da45f996c5a722d5669
hbridge <hbridge@gmail.com>
2014-12-08 12:39:23 -0500

    Add new VC and assets

---
+++ strand/iOS/Strand/Strand/DFIncomingViewController.h
@@ -0,0 +1,13 @@
+//
+//  DFIncomingViewController.h
+//  Strand
+//
+//  Created by Henry Bridge on 12/8/14.
+//  Copyright (c) 2014 Duffy Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DFIncomingViewController : UIViewController
+
+@end
---
+++ strand/iOS/Strand/Strand/DFIncomingViewController.m
@@ -0,0 +1,37 @@
+//
+//  DFIncomingViewController.m
+//  Strand
+//
+//  Created by Henry Bridge on 12/8/14.
+//  Copyright (c) 2014 Duffy Inc. All rights reserved.
+//
+
+#import "DFIncomingViewController.h"
+
+@interface DFIncomingViewController ()
+
+@end
+
+@implementation DFIncomingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
