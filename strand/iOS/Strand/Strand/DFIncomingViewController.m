19f4668dc2dffe4d78e69fe6d078cc1cc193f48e
Derek Parham <dparham@gmail.com>
2014-12-19 16:09:22 -0500

    don't have incoming listen to new data

--- strand/iOS/Strand/Strand/DFIncomingViewController.m
+++ strand/iOS/Strand/Strand/DFIncomingViewController.m
@@ -55,17 +55,6 @@
 {
   [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
   [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
-
-  [[NSNotificationCenter defaultCenter] addObserver:self
-                                           selector:@selector(reloadData)
-                                               name:DFStrandNewInboxDataNotificationName
-                                             object:nil];
-}
-
-- (void)reloadData
-{
-  [self configurePhotoDetailView];
-  [self configureSwipableButtonView];
 }
 
 - (void)configurePhotoDetailView
