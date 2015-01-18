4e55b89e1fcd6d29aca9130b27e661c6e48bb42b
asood123 <asood123@yahoo.com>
2015-01-18 18:22:12 +0000

    Cleaning up summary page for fast reloda

--- peanut/arbus/viz_views.py
+++ peanut/arbus/viz_views.py
@@ -21,27 +21,27 @@ from common.models import Photo, User, Classification, NotificationLog, Action, 
 
 from arbus import image_util, search_util
 from arbus.forms import ManualAddPhoto
+from common import stats_util
 
 logger = logging.getLogger(__name__)
 	
 def userbaseSummary(request):
 
+	# get now and always round up the hour to let database build its cache
+	now = datetime.utcnow().replace(tzinfo=pytz.utc)
+	newNow = now.replace(microsecond=0, second=0, minute=0)+timedelta(hours=1)
+
 	# database calls
 	userStats = list(User.objects.filter(product_id=2))
-	photoDataRaw = list(Photo.objects.exclude(id__lt=7463).exclude(Q(install_num=-1) & Q(thumb_filename = None)).values('user').annotate(
-		totalPhotos=Count('user'), thumbsCount=Count('thumb_filename'), photosWithGPS=Count('location_point'), lastUpdated=Max('updated')))
-
-	actionDataRaw = list(Action.objects.exclude(added__lt=(datetime.now()-timedelta(hours=168))).filter(Q(action_type=constants.ACTION_TYPE_PHOTO_EVALUATED) | Q(action_type=constants.ACTION_TYPE_FAVORITE) | Q(action_type=constants.ACTION_TYPE_COMMENT)).values('user', 'action_type').annotate(weeklyActions=Count('user'), lastActionTimestamp=Max('added')))
-
-	siDataForWeeklyPhotos = list(ShareInstance.objects.exclude(shared_at_timestamp__lt=(datetime.now()-timedelta(hours=168))).values('user').annotate(weeklyPhotosShared=Count('user')))
+	actionDataRaw = list(Action.objects.exclude(added__lt=(newNow-timedelta(hours=168))).filter(Q(action_type=constants.ACTION_TYPE_PHOTO_EVALUATED) | Q(action_type=constants.ACTION_TYPE_FAVORITE) | Q(action_type=constants.ACTION_TYPE_COMMENT)).values('user', 'action_type').annotate(weeklyActions=Count('user'), lastActionTimestamp=Max('added')))
+	siDataForWeeklyPhotos = list(ShareInstance.objects.exclude(shared_at_timestamp__lt=(newNow-timedelta(hours=168))).values('user').annotate(weeklyPhotosShared=Count('user')))
 	siDataForAllPhotos = list(ShareInstance.objects.values('user').annotate(allPhotosShared=Count('user')))
-
 	locationData = list(LocationRecord.objects.values('user').annotate(lastUpdated=Max('updated')))
 	contactCount = list(User.objects.filter(product_id=2).annotate(totalContacts=Count('contactentry')).order_by('-id'))
 	friendCount = list(User.objects.filter(product_id=2).annotate(totalFriends1=Count('friend_user_1', distinct=True), totalFriends2=Count('friend_user_2', distinct=True)).order_by('-id'))
 
 	# Exclude type GPS fetch since it happens so frequently
-	notificationDataRaw = list(NotificationLog.objects.filter(result=constants.IOS_NOTIFICATIONS_RESULT_SENT).exclude(msg_type=constants.NOTIFICATIONS_FETCH_GPS_ID).exclude(msg_type=constants.NOTIFICATIONS_REFRESH_FEED).exclude(added__lt=(datetime.now()-timedelta(hours=168))).values('user').order_by().annotate(totalNotifs=Count('user'), lastSent=Max('added')))
+	notificationDataRaw = list(NotificationLog.objects.filter(result=constants.IOS_NOTIFICATIONS_RESULT_SENT).exclude(msg_type=constants.NOTIFICATIONS_FETCH_GPS_ID).exclude(msg_type=constants.NOTIFICATIONS_REFRESH_FEED).exclude(added__lt=(newNow-timedelta(hours=168))).values('user').order_by().annotate(totalNotifs=Count('user'), lastSent=Max('added')))
 
 	extras = dict() #store additional information per user
 	for i in range(len(userStats)):
@@ -62,9 +62,6 @@ def userbaseSummary(request):
 	allSiById = dict()
 	locationById = dict()
 	
-	for photosData in photoDataRaw:
-		PhotosDataById[photosData['user']] = photosData
-
 	for actionData in siDataForWeeklyPhotos:
 		weeklyPhotosById[actionData['user']] = actionData['weeklyPhotosShared']
 
@@ -78,7 +75,6 @@ def userbaseSummary(request):
 	weeklyCommentsById = dict() #action_type = 4
 	weeklyPhotoEvalsById = dict() #action_type = 5
 	lastActionTimeById = dict()
-
 
 	for actionData in actionDataRaw:
 		if (actionData['action_type'] == constants.ACTION_TYPE_FAVORITE):
@@ -98,11 +94,6 @@ def userbaseSummary(request):
 
 	swapUserList = list()
 	to_zone = tz.gettz('America/New_York')
-	peopleCounts = dict()
-	peopleCounts['photosMetadata'] = 0
-	peopleCounts['photosShared'] = 0
-	peopleCounts['friends'] = 0
-
 	for i, user in enumerate(userStats):
 		entry = dict()
 		entry['user'] = user
@@ -114,17 +105,6 @@ def userbaseSummary(request):
 		if (user.last_checkin_timestamp):
 			entry['lastCheckinTime'] = user.last_checkin_timestamp.astimezone(to_zone).strftime('%Y/%m/%d %H:%M:%S')			
 
-		if (user.id in PhotosDataById):
-			user.totalCount = PhotosDataById[user.id]['totalPhotos']
-			user.thumbsCount = PhotosDataById[user.id]['thumbsCount']
-			user.photosWithGPS = PhotosDataById[user.id]['photosWithGPS']
-			user.lastUpdated = PhotosDataById[user.id]['lastUpdated']
-		else:
-			user.totalCount = 0
-			user.thumbsCount = 0
-			user.photoWithGPS = 0
-			user.lastUpdated = 0
-
 		if user.id in allSiById:
 			user.siCount = allSiById[user.id]
 		else:
@@ -132,12 +112,6 @@ def userbaseSummary(request):
 
 		if user.id in locationById:
 			user.lastLocationTimestamp = locationById[user.id]
-
-		if (user.totalCount > 0):
-			entry['lastUploadTime'] = user.lastUpdated.astimezone(to_zone).strftime('%Y/%m/%d %H:%M:%S')
-			entry['metadataCount'] = user.totalCount
-			entry['photosWithGPS'] = 100.0*float(user.photosWithGPS)/float(user.totalCount)
-
 
 		if user.id in notificationCountById:
 			entry['notifications'] = notificationCountById[user.id]
@@ -197,62 +171,19 @@ def userbaseSummary(request):
 					entry['internal'] = True
 					break
 
-		peopleCounts['photosMetadata'] += user.totalCount
-		peopleCounts['photosShared'] += user.thumbsCount
 		peopleCounts['friends'] += entry['friendCount']
 
 		swapUserList.append(entry)
 
 	swapUserList = sorted(swapUserList, key=lambda x: x['lastActionTimestamp'], reverse=True)
 
-	# database calls for top tables
-	shareInstances = ShareInstance.objects.prefetch_related('users')
-
-	# stats on # of users per share_instance
-	userBucket1 = userBucket2 = userBucket3 = userBucket4 = userBucket5 = 0
-	
-	for si in shareInstances:
-		if si.users.count() == 2:
-			userBucket1 += 1
-		elif si.users.count() == 3:
-			userBucket2 += 1
-		elif si.users.count() == 4:
-			userBucket3 += 1
-		elif si.users.count() == 5:
-			userBucket4 += 1
-		else:
-			userBucket5 += 1
-
-	userCounts = dict()
-	userCounts['all'] = len(shareInstances)
-	userCounts['b1'] = userBucket1
-	userCounts['b2'] = userBucket2
-	userCounts['b3'] = userBucket3
-	userCounts['b4'] = userBucket4
-	userCounts['b5'] = userBucket5
-
-	# calculate percentages
-	userCounts['b1p'] = float(userBucket1)/float(userCounts['all'])*100.0
-	userCounts['b2p'] = float(userBucket2)/float(userCounts['all'])*100.0
-	userCounts['b3p'] = float(userBucket3)/float(userCounts['all'])*100.0
-	userCounts['b4p'] = float(userBucket4)/float(userCounts['all'])*100.0
-	userCounts['b5p'] = float(userBucket5)/float(userCounts['all'])*100.0
-
-
-	peopleCounts['friends'] = peopleCounts['friends']/2 #dividing by two to count relationships
 	peopleCounts['all'] = len(swapUserList)
+	peopleCounts['friends'] = peopleCounts['friends']/2
 
-	statsCounts = dict()
-	statsCounts['friendsPerUser'] = float(peopleCounts['friends'])/float(peopleCounts['all'])
-	statsCounts['sisPerUser'] = float(userCounts['all'])/float(peopleCounts['all'])
-	statsCounts['photosSharedPerUser'] = float(peopleCounts['photosShared'])/float(peopleCounts['all'])
-	statsCounts['photosMetadataPerUser'] = float(peopleCounts['photosMetadata'])/float(peopleCounts['all'])
-	statsCounts['percentPhotosShared'] = float(peopleCounts['photosShared'])/float(peopleCounts['photosMetadata'])*100.0
+	peopleCounts['totalShareInstances'] = ShareInstance.objects.all().count()
 
 	context = {	'swapUserList': swapUserList,
-				'userCounts': userCounts,
-				'peopleCounts': peopleCounts,
-				'statsCounts': statsCounts}
+				'peopleCounts': peopleCounts}
 
 	return render(request, 'admin/userbaseSummary.html', context)
 
