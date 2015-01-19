1f606afb7303cafec71949fd5fe338fc3c8cfd0f
asood123 <asood123@yahoo.com>
2015-01-19 19:50:25 +0000

    added 3 hour time ping to users who haven't reported location recently

--- peanut/scripts/sendStrandNotifications.py
+++ peanut/scripts/sendStrandNotifications.py
@@ -15,7 +15,7 @@ from django.db.models import Count
 from django.db.models import Q
 
 from peanut.settings import constants
-from common.models import ShareInstance, User
+from common.models import ShareInstance, User, LocationRecord, NotificationLog
 
 from common import api_util
 
@@ -91,42 +91,81 @@ def main(argv):
 	logger.info("Starting... ")
 	notificationTimedelta = datetime.timedelta(seconds=300)
 	recencyTimedelta = datetime.timedelta(seconds=60)
+
+	locationSmallTimedelta = datetime.timedelta(hours=3)
+	locationBigTimedelta = datetime.timedelta(days=3)
 	
 	while True:
 		now = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
 		notificationsSent = list()
 
-		shareInstancesSent = ShareInstance.objects.filter(notification_sent__isnull=False).filter(added__gt=now-recencyTimedelta)
+		############################################
+		### Notifications for new shareInstances ###
+		############################################
+
+		# get all pending shareInstances that users haven't been notified about 
 		shareInstancesPending = ShareInstance.objects.filter(notification_sent__isnull=True).filter(added__gt=now-notificationTimedelta).filter(photo__full_filename__isnull=False)
 
-		usersSentNotsRecently = [si.user.id for si in shareInstancesSent]
-		if len(usersSentNotsRecently) > 0:
-			logger.debug("Users who sent a notification recently: %s"%(str(usersSentNotsRecently)))
+		# Process pending shareInstances
+		if len(shareInstancesPending) > 0:
+			shareInstancesSent = ShareInstance.objects.filter(notification_sent__isnull=False).filter(added__gt=now-recencyTimedelta)
+			usersSentNotsRecently = [si.user.id for si in shareInstancesSent]
+			if len(usersSentNotsRecently) > 0:
+				logger.debug("Users who sent a notification recently: %s"%(str(usersSentNotsRecently)))
 
-		siByUser = dict()
+			siByUser = dict()
 
-		for si in shareInstancesPending:
-			if si.user.id in usersSentNotsRecently:
-				# skip if this user has sent a shareInstance (that we notified someone of) recently
-				logger.info("skipping userId %s"%(si.user.id))
-				continue
-			else:
-				notificationsSent.append(si)
-				si.notification_sent = now
-				for user in si.users.all():
-					if user != si.user:
-						if user in siByUser:
-							siByUser[user].append(si)
-						else:
-							siByUser[user] = [si]
+			for si in shareInstancesPending:
+				if si.user.id in usersSentNotsRecently:
+					# skip if this user has sent a shareInstance (that we notified someone of) recently
+					logger.info("skipping userId %s"%(si.user.id))
+					continue
+				else:
+					notificationsSent.append(si)
+					si.notification_sent = now
+					for user in si.users.all():
+						if user != si.user:
+							if user in siByUser:
+								siByUser[user].append(si)
+							else:
+								siByUser[user] = [si]
 
-		for user, siList in siByUser.items():
-			sendNewPhotoNotificationBatch(user, siList)
+			for user, siList in siByUser.items():
+				sendNewPhotoNotificationBatch(user, siList)
 
-		if len(notificationsSent) > 0:
-			ShareInstance.bulkUpdate(notificationsSent, ['notification_sent'])
-		else:
-			time.sleep(1)
+			if len(notificationsSent) > 0:
+				ShareInstance.bulkUpdate(notificationsSent, ['notification_sent'])
+
+		############################################
+		### Notifications for updating location  ###
+		############################################
+
+		# get the list of users who have given us a past location and ping them
+		usersInLocTable = LocationRecord.objects.filter(added__gt=now-locationBigTimedelta).values('user').distinct()
+		userIds = [entry['user'] for entry in usersInLocTable]
+		usersRecentlyInLocTable = LocationRecord.objects.filter(added__gt=now-locationSmallTimedelta).values('user').distinct()
+
+		# Remove any userIds that have sent location in last 3 hours
+		for entry in usersRecentlyInLocTable:
+			if entry['user'] in userIds:
+				userIds.remove(entry['user'])
+
+		# Check to make sure that we haven't sent them a location ping in last 3 hours already
+		recentlyPingedUsers = NotificationLog.objects.filter(added__gt=now-locationSmallTimedelta).filter(msg_type=constants.NOTIFICATIONS_FETCH_GPS_ID).values('user').distinct()
+
+		for entry in recentlyPingedUsers:
+			if entry['user'] in userIds:
+				userIds.remove(entry['user']) 
+
+		if len(userIds) > 0: #location ping
+			logger.info("Found users to ping for 3 hour check: %s"%(userIds))
+			users = User.objects.filter(id__in=userIds)
+			for user in users:
+				logger.debug("going to send a Fetch_GPS_ID to user id %s" % (user.id))
+				customPayload = {}
+				notifications_util.sendNotification(user, '', constants.NOTIFICATIONS_FETCH_GPS_ID, customPayload)
+		
+		time.sleep(1)
 
 if __name__ == "__main__":
 	logging.basicConfig(filename='/var/log/duffy/strand-notifications.log',
