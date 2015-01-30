19c37f1c3d5dcd2884a85e723a400ffb03b1576f
Derek Parham <dparham@gmail.com>
2015-01-30 17:09:59 +0000

    celery startup and shutdown script

--- peanut/scripts/celery.sh
+++ peanut/scripts/celery.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-export DJANGO_SETTINGS_MODULE=peanut.settings.dev
+
+if [ "$2" == "dev" ]
+then
+	export DJANGO_SETTINGS_MODULE=peanut.settings.dev
+	cd /home/ubuntu/dev/Duffy/peanut
+elif [[ "$2" == "prod" ]]; then
+	export DJANGO_SETTINGS_MODULE=peanut.settings.prod
+	cd /home/ubuntu/Duffy/peanut
+fi
+
 if [ "$1" == "start" ]
 then
-	python manage.py celeryd_multi start independent stranding popcaches ordered_low -B --logfile=/mnt/log/celery-%N.log --pidfile=/mnt/log/celery-%N.pid -c 1 -c:independent 10 -Q:independent independent -Q:stranding stranding -Q:popcaches popcaches -Q:ordered_low ordered_low
+	/home/ubuntu/env/bin/python manage.py celeryd_multi start independent stranding popcaches ordered_low -B --logfile=/mnt/log/celery-%N.log --pidfile=/mnt/run/celery-%N.pid -c 1 -c:independent 10 -Q:independent independent -Q:stranding stranding -Q:popcaches popcaches -Q:ordered_low ordered_low
 else
-	python manage.py celeryd_multi stop independent stranding popcaches ordered_low -B --logfile=/mnt/log/celery-%N.log --pidfile=/mnt/log/celery-%N.pid
+	/home/ubuntu/env/bin/python manage.py celeryd_multi stop independent stranding popcaches ordered_low -B --logfile=/mnt/log/celery-%N.log --pidfile=/mnt/run/celery-%N.pid
 fi	
