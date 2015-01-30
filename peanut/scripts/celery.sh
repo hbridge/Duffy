bdec8121366627555a8d340289e021df3edc0b68
Derek Parham <dparham@gmail.com>
2015-01-30 00:15:05 +0000

    try setting env

--- peanut/scripts/celery.sh
+++ peanut/scripts/celery.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+export DJANGO_SETTINGS_MODULE=peanut.settings.dev
 if [ "$1" == "start" ]
 then
 	python manage.py celeryd_multi start independent stranding popcaches ordered_low -B --logfile=/mnt/log/celery-%N.log --pidfile=/mnt/log/celery-%N.pid -c 1 -c:independent 10 -Q:independent independent -Q:stranding stranding -Q:popcaches popcaches -Q:ordered_low ordered_low
