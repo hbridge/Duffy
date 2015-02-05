b40e6d4f020a6550a4b1a6cd8f1e6aacb738f8d0
hbridge <hbridge@gmail.com>
2015-02-05 18:11:45 -0500

    Scale NUX for larger screens

--- strand/iOS/Strand/Strand/DFOverlayNUXViewController.m
+++ strand/iOS/Strand/Strand/DFOverlayNUXViewController.m
@@ -33,7 +33,17 @@
                                                   [UIColor colorWithRedByte:228 green:228 blue:228 alpha:1.0],
                                                   [UIColor colorWithRedByte:255 green:255 blue:255 alpha:1.0]
                                                   ];
+  self.topImageView.backgroundColor = [UIColor colorWithRedByte:105 green:160 blue:224 alpha:1.0];
   [self configureContent];
+  [self configureFontSizes];
+}
+
+- (void)viewDidLayoutSubviews
+{
+  [super viewDidLayoutSubviews];
+  for (UILabel *label in @[self.subtitleLabel, self.explanatoryTextLabel]) {
+    label.preferredMaxLayoutWidth = label.frame.size.width;
+  }
 }
 
 - (void)configureContent
@@ -57,6 +67,36 @@
   if (error) {
     DDLogError(@"%@ error parsing markup: %@", self.class, error);
   }
+}
+
+- (void)configureFontSizes
+{
+  CGFloat fontSize = [UIFont systemFontSize]*[UIScreen mainScreen].bounds.size.width/240;
+  CGFloat subFontSize = fontSize * 0.8;
+  self.titleLabel.font = [self.titleLabel.font fontWithSize:fontSize * 1.1];
+  self.subtitleLabel.font = [self.subtitleLabel.font fontWithSize:subFontSize];
+  
+  
+  NSMutableAttributedString *res = [self.explanatoryTextLabel.attributedText mutableCopy];
+  
+  [res beginEditing];
+  __block BOOL found = NO;
+  [res enumerateAttribute:NSFontAttributeName inRange:NSMakeRange(0, res.length) options:0 usingBlock:^(id value, NSRange range, BOOL *stop) {
+    if (value) {
+      UIFont *oldFont = (UIFont *)value;
+      UIFont *newFont = [oldFont fontWithSize:subFontSize];
+      [res removeAttribute:NSFontAttributeName range:range];
+      [res addAttribute:NSFontAttributeName value:newFont range:range];
+      found = YES;
+    }
+  }];
+  if (!found) {
+    // No font was found - do something else?
+  }
+  [res endEditing];
+  
+  
+  self.explanatoryTextLabel.attributedText = res;
 }
 
 
