954fe1a4bea3e1142d6f95bf566943517464a83a
Derek Parham <dparham@gmail.com>
2015-02-06 19:09:14 +0000

    fix typo

--- peanut/common/serializers.py
+++ peanut/common/serializers.py
@@ -97,7 +97,7 @@ def objectDataForShareInstance(shareInstance, actions, user):
 	shareInstanceData['full_height'] = shareInstance.photo.full_height
 
 	# Now filter out anything that doesn't have a thumb...unless its your own photo
-	if not hareInstance.photo.thumb_filename and shareInstance.user_id != user.id:
+	if not shareInstance.photo.thumb_filename and shareInstance.user_id != user.id:
 		return None
 
 	publicActions = list()
