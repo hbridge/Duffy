24fd853d41c7b64cc5b4e7e47e3b127a2a842288
Derek Parham <dparham@gmail.com>
2015-02-11 17:12:58 +0000

    try prod settings for dev

--- peanut/peanut/settings/dev.py
+++ peanut/peanut/settings/dev.py
@@ -45,7 +45,7 @@ EMAIL_USE_TLS = True
 SERVER_EMAIL = 'dev@duffyapp.com'
 
 ADMINS = (
-	('', ''),
+	('Admins', 'server-errors@duffytech.co'),
 )
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "prod.strand.duffyapp.com", "dev.duffyapp.com"]
