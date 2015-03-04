b43f034c6a57de14e53e0ddb5f6543de9808864e
Derek Parham <dparham@gmail.com>
2015-03-04 22:56:16 +0000

    Add in jsonp support

--- peanut/strand/api_views.py
+++ peanut/strand/api_views.py
@@ -48,6 +48,39 @@ def getAllIdsInFeedObjects(feedObjects):
 			peopleIds.update(getAllIdsInFeedObjects(obj["objects"]))
 	return list(peopleIds)
 
+def jsonp(f):
+	"""Wrap a json response in a callback, and set the mimetype (Content-Type) header accordingly 
+	(will wrap in text/javascript if there is a callback). If the "callback" or "jsonp" paramters 
+	are provided, will wrap the json output in callback({thejson})
+	
+	Usage:
+	
+	@jsonp
+	def my_json_view(request):
+		d = { 'key': 'value' }
+		return HTTPResponse(json.dumps(d), content_type='application/json')
+	
+	"""
+	from functools import wraps
+	@wraps(f)
+	def jsonp_wrapper(request, *args, **kwargs):
+		resp = f(request, *args, **kwargs)
+		if resp.status_code != 200:
+			return resp
+		if 'callback' in request.GET:
+			callback= request.GET['callback']
+			resp['Content-Type']='text/javascript; charset=utf-8'
+			resp.content = "%s(%s)" % (callback, resp.content)
+			return resp
+		elif 'jsonp' in request.GET:
+			callback= request.GET['jsonp']
+			resp['Content-Type']='text/javascript; charset=utf-8'
+			resp.content = "%s(%s)" % (callback, resp.content)
+			return resp
+		else:
+			return resp                
+				
+	return jsonp_wrapper
 
 #####################################################################################
 #################################  EXTERNAL METHODS  ################################
@@ -225,7 +258,7 @@ def actions_list(request):
 	return HttpResponse(json.dumps(response, cls=api_util.DuffyJsonEncoder), content_type="application/json")
 
 #   -------------------------  OTHER ENDPOINTS ---------------------
-
+@jsonp
 def website_registration(request):
 	response = dict({'result': True})
 	form = WebsiteRegistrationForm(api_util.getRequestData(request))
