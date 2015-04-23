df85ed002b19592aa1daa1a0ae86fcb5fdfd3fbf
Derek Parham <dparham@gmail.com>
2015-04-23 21:42:45 +0000

    Support for south on prod pushes

--- peanut/scripts/prodPush.sh
+++ peanut/scripts/prodPush.sh
@@ -3,6 +3,8 @@
 ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "cd Duffy/peanut; git fetch; git rebase origin/master"
 ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "sudo apachectl -k restart"
 
+ssh -i ~derek/derek-key-pair-east.pem ubuntu@db.prod.strand.duffyapp.com "cd Duffy/peanut; python manage.py syncdb" 
+
 export DJANGO_SETTINGS_MODULE=peanut.settings.prod
 python /home/ubuntu/dev/Duffy/peanut/tests/black_box_api_tests.py prod.strand.duffyapp.com 5020
 export DJANGO_SETTINGS_MODULE=peanut.settings.dev
