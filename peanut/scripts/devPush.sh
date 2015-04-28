4b5f406db3d6e715fdeb628598b251ce740e2478
Derek Parham <dparham@gmail.com>
2015-04-28 19:27:06 +0000

    Stop calling swap unit tests

--- peanut/scripts/devPush.sh
+++ peanut/scripts/devPush.sh
@@ -14,7 +14,7 @@ sudo start duffy-celery
 sudo stop duffy-celery-beat
 sudo start duffy-celery-beat
 
-source /home/ubuntu/env/bin/activate; python /home/ubuntu/dev/Duffy/peanut/tests/black_box_api_tests.py dev.duffyapp.com 653
+#source /home/ubuntu/env/bin/activate; python /home/ubuntu/dev/Duffy/peanut/tests/black_box_api_tests.py dev.duffyapp.com 653
 
 initctl list | grep duffy-strand-notifications
 initctl list | grep duffy-celery
--- peanut/scripts/prodPush.sh
+++ peanut/scripts/prodPush.sh
@@ -6,9 +6,9 @@ ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "sudo apac
 # Doesn't work due to environment issues, do manually for now
 #ssh -i ~derek/derek-key-pair-east.pem ubuntu@db.prod.strand.duffyapp.com "source ~/.bashrc; cd Duffy/peanut; python manage.py syncdb" 
 
-export DJANGO_SETTINGS_MODULE=peanut.settings.prod
-python /home/ubuntu/dev/Duffy/peanut/tests/black_box_api_tests.py prod.strand.duffyapp.com 5020
-export DJANGO_SETTINGS_MODULE=peanut.settings.dev
+#export DJANGO_SETTINGS_MODULE=peanut.settings.prod
+#python /home/ubuntu/dev/Duffy/peanut/tests/black_box_api_tests.py prod.strand.duffyapp.com 5020
+#export DJANGO_SETTINGS_MODULE=peanut.settings.dev
 
 echo "Restarting scripts..."
 ssh -i ~derek/derek-key-pair-east.pem ubuntu@prod.strand.duffyapp.com "sudo restart duffy-strand-notifications"
