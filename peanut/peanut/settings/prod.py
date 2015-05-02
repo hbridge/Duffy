07f95a18a086f09d73466521c707da0d3b91256c
Derek Parham <dparham@gmail.com>
2015-05-02 16:25:01 +0000

    Turn error emailing back on

--- peanut/peanut/settings/prod.py
+++ peanut/peanut/settings/prod.py
@@ -35,7 +35,7 @@ EMAIL_USE_TLS = True
 SERVER_EMAIL = 'prod@duffyapp.com'
 
 ADMINS = (
-	('Admins'), #, 'server-errors@duffytech.co'),
+	('Admins', 'server-errors@duffytech.co'),
 )
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "prod.strand.duffyapp.com"]
