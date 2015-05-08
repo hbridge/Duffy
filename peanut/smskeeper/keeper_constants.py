3d428098c863bd5cca01011ed641f25fb65076ec
hbridge <hbridge@gmail.com>
2015-05-08 11:49:48 -0400

    change text

--- peanut/smskeeper/keeper_constants.py
+++ peanut/smskeeper/keeper_constants.py
@@ -18,4 +18,4 @@ STATE_ADD = 'add'
 
 UNASSIGNED_LABEL = '#unassigned'
 REMIND_LABEL = "#reminders"
-GENERIC_ERROR_MESSAGE = '\xF0\x9F\x98\xB2 something went wrong.  My overlords have been notified.'
+GENERIC_ERROR_MESSAGE = '\xF0\x9F\x98\xB2 something went wrong.  My minions have been notified.'
