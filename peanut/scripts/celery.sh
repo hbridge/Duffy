bc869316e3b85da8ca823d96c9f6e142ad2de9b4
Derek Parham <dparham@gmail.com>
2015-05-13 21:37:44 +0000

    Fix bug where celery wasn't starting

--- peanut/scripts/celery.sh
+++ peanut/scripts/celery.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Really really need this because the upstart script runs as another user so need to setup the virtualenv
+PYTHON=/home/ubuntu/env/bin/python
+
 if [ "$2" == "dev" ]
 then
 	export DJANGO_SETTINGS_MODULE=peanut.settings.dev
@@ -10,13 +13,14 @@ elif [[ "$2" == "prod" ]]; then
 elif [[ "$2" == "local" ]]; then
 	export DJANGO_SETTINGS_MODULE=peanut.settings.local
 	cd /Users/hbridge/Documents/Repos/Duffy/peanut
+	PYTHON=python
 fi
 
 if [ "$1" == "start" ]
 then
-	python manage.py celeryd_multi start keeper --logfile=/mnt/log/celery-%N.log --pidfile=/mnt/run/celery-%N.pid -c 1 -c:keeper 5 -Q:keeper keeper
+	$PYTHON manage.py celeryd_multi start keeper --logfile=/mnt/log/celery-%N.log --pidfile=/mnt/run/celery-%N.pid -c 1 -c:keeper 5 -Q:keeper keeper
 else
-	python manage.py celeryd_multi stop keeper --logfile=/mnt/log/celery-%N.log --pidfile=/mnt/run/celery-%N.pid
+	$PYTHON manage.py celeryd_multi stop keeper --logfile=/mnt/log/celery-%N.log --pidfile=/mnt/run/celery-%N.pid
 fi
 
 if [ "$1" == "killall" ]
