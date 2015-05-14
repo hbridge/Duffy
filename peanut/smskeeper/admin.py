965496e84a869c82aeec544601de6876e6414f2c
Derek Parham <dparham@gmail.com>
2015-05-14 23:13:20 +0000

    Update admin page

--- peanut/smskeeper/admin.py
+++ peanut/smskeeper/admin.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 from django.contrib import admin
 
-from smskeeper.models import User
+from smskeeper.models import User, Entry
 from smskeeper import keeper_constants
 from smskeeper import user_util
 
@@ -23,3 +23,5 @@ activate_to_list.short_description = "Activate to List Tutorial"
 class UserAdmin(admin.ModelAdmin):
 	list_display = ('id', 'activated', 'phone_number', 'name', 'state', 'completed_tutorial', 'state_data', 'print_last_message_date', 'total_msgs_from', 'history')
 	actions = [activate_to_remind, activate_to_list]
+
+admin.site.register(Entry)
