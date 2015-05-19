0dc698d96b4c9883e3396584cfee360a75d5b8ed
Derek Parham <dparham@gmail.com>
2015-05-19 20:22:13 +0000

    Derek config file

---
+++ peanut/peanut/settings/derek.py
@@ -0,0 +1,8 @@
+from peanut.settings.dev import *
+
+STATICFILES_DIRS = (
+	# Put strings here, like "/home/html/static" or "C:/www/django/static".
+	# Always use forward slashes, even on Windows.
+	# Don't forget to use absolute paths, not relative paths.
+	'/home/derek/Duffy/peanut/static',
+)
