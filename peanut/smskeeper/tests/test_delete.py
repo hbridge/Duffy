007dab6106312e24b4788e12d118dcd3199c5223
hbridge <hbridge@gmail.com>
2015-05-26 16:13:15 -0400

    wire up delete tests

--- peanut/smskeeper/tests/test_delete.py
+++ peanut/smskeeper/tests/test_delete.py
@@ -1,39 +1,115 @@
-def test_freeform_absolute_delete(self):
-	self.setupUser(True, True, keeper_constants.STATE_NORMAL)
+from mock import patch
 
-	cliMsg.msg(self.testPhoneNumber, "Add milk, spinach, bread to groceries")
-	cliMsg.msg(self.testPhoneNumber, "Delete 1 from groceries")
-	with patch('smskeeper.async.recordOutput') as mock:
+from smskeeper import msg_util, cliMsg, keeper_constants
+
+import test_base
+
+
+class SMSKeeperDeleteCase(test_base.SMSKeeperBaseCase):
+	def test_delete_parsing(self):
+		self.assertTrue(msg_util.isDeleteCommand("delete 1"))
+		self.assertTrue(msg_util.isDeleteCommand("delete 1 from groceries"))
+		self.assertTrue(msg_util.isDeleteCommand("delete 1 #groceries"))
+
+		label, indices = msg_util.parseDeleteCommand("delete 1")
+		self.assertEqual(label, None)
+		self.assertEqual(indices, set([1]))
+
+		label, indices = msg_util.parseDeleteCommand("delete 1,2,3 from groceries")
+		self.assertEqual(label, "#groceries")
+		self.assertEqual(indices, set([1, 2, 3]))
+
+	def test_freeform_absolute_delete(self):
+		self.setupUser(True, True, keeper_constants.STATE_NORMAL)
+
+		cliMsg.msg(self.testPhoneNumber, "Add milk, spinach, bread to groceries")
+		cliMsg.msg(self.testPhoneNumber, "Delete 1 from groceries")
+		with patch('smskeeper.async.recordOutput') as mock:
+			cliMsg.msg(self.testPhoneNumber, "Groceries")
+			self.assertNotIn("milk", self.getOutput(mock))
+
+	def test_freeform_delete(self):
+		self.setupUser(True, True, keeper_constants.STATE_NORMAL)
+
+		cliMsg.msg(self.testPhoneNumber, "Add milk, spinach, bread to groceries")
 		cliMsg.msg(self.testPhoneNumber, "Groceries")
-		self.assertNotIn("milk", getOutput(mock))
+		cliMsg.msg(self.testPhoneNumber, "Delete 1")
+		with patch('smskeeper.async.recordOutput') as mock:
+			cliMsg.msg(self.testPhoneNumber, "Groceries")
+			self.assertNotIn("milk", self.getOutput(mock))
 
-def test_delete_parsing(self):
-	self.assertTrue(msg_util.isDeleteCommand("delete 1"))
-	self.assertTrue(msg_util.isDeleteCommand("delete 1 from groceries"))
-	self.assertTrue(msg_util.isDeleteCommand("delete 1 #groceries"))
+	def test_absolute_delete(self):
+		self.setupUser(True, True)
+		# ensure deleting from an empty list doesn't crash
+		cliMsg.msg(self.testPhoneNumber, "delete 1 #test")
+		cliMsg.msg(self.testPhoneNumber, "old fashioned #cocktail")
 
-	label, indices = msg_util.parseDeleteCommand("delete 1")
-	self.assertEqual(label, None)
-	self.assertEqual(indices, set([1]))
+		# First make sure that the entry is there
+		with patch('smskeeper.async.recordOutput') as mock:
+			cliMsg.msg(self.testPhoneNumber, "#cocktail")
+			self.assertIn("old fashioned", self.getOutput(mock))
 
-	label, indices = msg_util.parseDeleteCommand("delete 1,2,3 from groceries")
-	self.assertEqual(label, "#groceries")
-	self.assertEqual(indices, set([1, 2, 3]))
+		# Next make sure we delete and the list is clear
+		cliMsg.msg(self.testPhoneNumber, "delete 1 #cocktail")   # test absolute delete
+		with patch('smskeeper.async.recordOutput') as mock:
+			cliMsg.msg(self.testPhoneNumber, "#cocktail")
+			self.assertNotIn("old fashioned", self.getOutput(mock))
 
-def test_contextual_clear(self):
-	self.setupUser(True, True)
-	for i in range(1, 2):
-		cliMsg.msg(self.testPhoneNumber, "foo%d #bar" % (i))
+	def test_contextual_delete(self):
+		self.setupUser(True, True)
+		for i in range(1, 2):
+			cliMsg.msg(self.testPhoneNumber, "add foo%d to #bar" % (i))
 
-	# ensure we don't clear this list when we use an absolute of another list
-	cliMsg.msg(self.testPhoneNumber, "bar")
-	cliMsg.msg(self.testPhoneNumber, "clear baz")
-	with patch('smskeeper.async.recordOutput') as mock:
+		# ensure we don't delete when ambiguous
+		with patch('smskeeper.async.recordOutput') as mock:
+			cliMsg.msg(self.testPhoneNumber, "delete 1")
+			self.assertIn("Sorry, I'm not sure", self.getOutput(mock))
+
+		# ensure deletes right item
+		cliMsg.msg(self.testPhoneNumber, "#bar")
+		with patch('smskeeper.async.recordOutput') as mock:
+			cliMsg.msg(self.testPhoneNumber, "delete 2")
+			self.assertNotIn("2. foo2", self.getOutput(mock))
+
+		# ensure can chain deletes
+		with patch('smskeeper.async.recordOutput') as mock:
+			cliMsg.msg(self.testPhoneNumber, "delete 1")
+			self.assertNotIn("1. foo1", self.getOutput(mock))
+
+		# ensure deleting from empty doesn't crash
+		with patch('smskeeper.async.recordOutput') as mock:
+			cliMsg.msg(self.testPhoneNumber, "delete 1")
+			self.assertNotIn("I deleted", self.getOutput(mock))
+
+	def test_contextual_clear(self):
+		self.setupUser(True, True)
+		for i in range(1, 2):
+			cliMsg.msg(self.testPhoneNumber, "foo%d #bar" % (i))
+
+		# ensure we don't clear this list when we use an absolute of another list
 		cliMsg.msg(self.testPhoneNumber, "bar")
-		self.assertIn("foo", getOutput(mock))
+		cliMsg.msg(self.testPhoneNumber, "clear baz")
+		with patch('smskeeper.async.recordOutput') as mock:
+			cliMsg.msg(self.testPhoneNumber, "bar")
+			self.assertIn("foo", self.getOutput(mock))
 
-	# clear now after we just listed bar
-	cliMsg.msg(self.testPhoneNumber, "clear")
-	with patch('smskeeper.async.recordOutput') as mock:
-		cliMsg.msg(self.testPhoneNumber, "bar")
-		self.assertNotIn("foo", getOutput(mock))
+		# clear now after we just listed bar
+		cliMsg.msg(self.testPhoneNumber, "clear")
+		with patch('smskeeper.async.recordOutput') as mock:
+			cliMsg.msg(self.testPhoneNumber, "bar")
+			self.assertNotIn("foo", self.getOutput(mock))
+
+	def test_multi_delete(self):
+		self.setupUser(True, True)
+		for i in range(1, 5):
+			cliMsg.msg(self.testPhoneNumber, "foo%d #bar" % (i))
+
+		# ensure we can delete with or without spaces
+		cliMsg.msg(self.testPhoneNumber, "delete 3, 5,2 #bar")
+
+		with patch('smskeeper.async.recordOutput') as mock:
+			cliMsg.msg(self.testPhoneNumber, "#bar")
+
+			self.assertNotIn("foo2", self.getOutput(mock))
+			self.assertNotIn("foo3", self.getOutput(mock))
+			self.assertNotIn("foo5", self.getOutput(mock))
