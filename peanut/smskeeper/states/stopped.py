ef75a4ee85af74f8bc39751315268702b4724d99
Derek Parham <dparham@gmail.com>
2015-05-26 22:11:46 +0000

    Support stopped state relive

--- peanut/smskeeper/states/stopped.py
+++ peanut/smskeeper/states/stopped.py
@@ -1,2 +1,13 @@
+from smskeeper import keeper_constants
+
+
 def process(user, msg, requestDict, keeperNumber):
-	return True
+	# If we get anything from them, start up again
+	user.setState(keeper_constants.STATE_NORMAL)
+	user.save()
+
+	# If we got the start message, then ignore
+	if msg.lower() == "start":
+		return True
+	# Otherwise, send through processing
+	return False
