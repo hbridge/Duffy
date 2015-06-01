00fed911e69ffb3daf5db14acf05753167a58bfa
asood123 <asood123@yahoo.com>
2015-06-01 18:48:04 +0000

    removing fb-7 with fb

--- peanut/smskeeper/views.py
+++ peanut/smskeeper/views.py
@@ -409,7 +409,7 @@ def signup_from_website(request):
 					else:
 						logger.debug("Didn't find any referrerCodes for code %s" % referrerCode)
 				else:
-					if source == "fb-7":
+					if source == "fb":
 						user_util.activate(target_user, "", None, settings.KEEPER_NUMBER)
 					else:
 						not_activated.dealWithNonActivatedUser(target_user, settings.KEEPER_NUMBER)
