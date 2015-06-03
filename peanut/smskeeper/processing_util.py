6e25b3ff0b15acb2dd7b923e4b0518e0503a973e
Derek Parham <dparham@gmail.com>
2015-06-03 20:08:36 +0000

    Make sure we override setstate for stoping things

--- peanut/smskeeper/processing_util.py
+++ peanut/smskeeper/processing_util.py
@@ -39,7 +39,7 @@ def processMessage(phoneNumber, msg, requestDict, keeperNumber):
 
 	# Always look for a stop command first and deal with that
 	if msg_util.isStopCommand(msg):
-		user.setState(keeper_constants.STATE_STOPPED, saveCurrent=True)
+		user.setState(keeper_constants.STATE_STOPPED, saveCurrent=True, override=True)
 		user.save()
 		logger.debug("I think '%s' is a stop command, setting state to %s for user %s" % (msg, user.state, user.id))
 
