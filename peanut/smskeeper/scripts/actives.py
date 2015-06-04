28ab59422d5b16fec32fb3a2ca870a126af807a9
asood123 <asood123@yahoo.com>
2015-06-04 17:58:21 +0000

    adding more code to actives.py

--- peanut/smskeeper/scripts/actives.py
+++ peanut/smskeeper/scripts/actives.py
@@ -17,3 +17,11 @@ print "Total users activated: %s (FB: %s)" %(len(activatedUsersAll), len(activat
 print "Active users: %s (FB: %s)"%(len(usersAll), len(usersFB))
 print "%% actives: %s%% (FB: %s%%)"%(float(len(usersAll))/float(len(activatedUsersAll))*100.0, float(len(usersFB))/float(len(activatedUsersFB))*100.0)
 print "Stopped users: %s"%(len(usersStopped))
+
+
+# query to get sorted list of incoming count by users
+
+users = Message.objects.values_list('user__id', 'user__name').filter(incoming=True, user__completed_tutorial=True).annotate(total_incoming=Count('user__id')).order_by('-total_incoming')
+
+for entry in users:
+    print "%s %s: %s"%(entry[0], entry[1], entry[2])
