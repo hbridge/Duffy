2e8c172bda2e7bc3b9137bf54f74c296b41544d7
asood123 <asood123@yahoo.com>
2015-06-08 21:17:21 +0000

    fixing encoding

--- peanut/smskeeper/scripts/massSMS.py
+++ peanut/smskeeper/scripts/massSMS.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-# This Python file uses the following encoding: utf-8
 import sys, os
 import time, datetime
 import pytz
@@ -25,7 +24,7 @@ def sendMassSMS(userIdList):
 	userList = User.objects.filter(id__in=userIdList)
 
 	for user in userList:
-		msg = u"Hi %s! I'm trying to go from 🐣 to 🐓. Do you have any tips for me on how I can help you more? getkeeper.com/feedback.php"%(user.name)
+		msg = u"Hi %s! I'm trying to go from \U0001F423 to \U0001F413. Do you have any tips for me on how I can help you more? getkeeper.com/feedback.php"%(user.name)
 		logger.debug("Sent msg to %s: %s"%(user.id, msg))
 		sendMsg(user, msg, None, settings.KEEPER_NUMBER)
 
