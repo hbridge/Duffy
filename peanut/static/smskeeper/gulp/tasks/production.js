13558aada89ebfda026bffef4456c03ee7127b00
hbridge <hbridge@gmail.com>
2015-06-12 11:24:42 -0400

    speed up react apps in prod

--- peanut/static/smskeeper/gulp/tasks/production.js
+++ peanut/static/smskeeper/gulp/tasks/production.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
 
-// Run this to compress all the things!
-gulp.task('production', ['karma'], function(){
-  // This runs only if the karma tests pass
-  gulp.start(['markup', 'images', 'iconFont', 'minifyCss', 'uglifyJs'])
+
+// set NODE_ENV so we use prod REACT
+process.env.NODE_ENV = 'production';
+gulp.task('production', function(){
+	gulp.start(['uglifyJs']);
 });
+
+// Run this to compress all the things!
+// gulp.task('production', ['karma'], function(){
+//   // This runs only if the karma tests pass
+//   gulp.start(['markup', 'images', 'iconFont', 'minifyCss', 'uglifyJs'])
+// });
