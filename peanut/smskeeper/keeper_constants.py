1625d098b4d18adc8ba7757e8f1c56c00c2a5c7e
asood123 <asood123@yahoo.com>
2015-07-02 19:45:50 +0000

    changing feedback prompt

--- peanut/smskeeper/keeper_constants.py
+++ peanut/smskeeper/keeper_constants.py
@@ -156,7 +156,7 @@ SHARE_UPSELL_FREQUENCY_DAYS = 3
 SHARE_UPSELL_PHRASE = "If you know anyone else who could use my help, send them to"
 
 FEEDBACK_FREQUENCY_DAYS = 15
-FEEDBACK_PHRASE = u"Any tips for me on how I can help you more? \U0001F423 getkeeper.com/feedback.php"
+FEEDBACK_PHRASE = u"I'd love to hear how I can do better. \U0001F423 getkeeper.com/feedback.php"
 FEEDBACK_MIN_ACTIVATED_TIME_IN_DAYS = 3
 
 SMSKEEPER_TEST_NUM = "test"
