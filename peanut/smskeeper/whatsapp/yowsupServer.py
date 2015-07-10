119e3d9420b17fbfd2e7e6555f6f5ae89f0d1528
hbridge <hbridge@gmail.com>
2015-07-10 16:13:10 -0400

    add additional layers to prevent disconnects

--- peanut/smskeeper/whatsapp/yowsupServer.py
+++ peanut/smskeeper/whatsapp/yowsupServer.py
@@ -5,22 +5,45 @@ from yowsup.layers.auth import YowAuthenticationProtocolLayer
 from yowsup.layers.protocol_messages import YowMessagesProtocolLayer
 from yowsup.layers.protocol_receipts import YowReceiptProtocolLayer
 from yowsup.layers.protocol_acks import YowAckProtocolLayer
+from yowsup.layers.protocol_media import YowMediaProtocolLayer
+from yowsup.layers.protocol_iq import YowIqProtocolLayer
+from yowsup.layers.protocol_calls import YowCallsProtocolLayer
 from yowsup.layers.network import YowNetworkLayer
 from yowsup.layers.coder import YowCoderLayer
+from yowsup.layers.axolotl import YowAxolotlLayer
+from yowsup.layers.logger import YowLoggerLayer
+from yowsup.layers.auth import YowCryptLayer
+from yowsup.layers.stanzaregulator import YowStanzaRegulator
+
 from yowsup.stacks import YowStack
 from yowsup.common import YowConstants
 from yowsup.layers import YowLayerEvent
-from yowsup.stacks import YOWSUP_CORE_LAYERS
 from yowsup import env
 
+# import logging
+# logging.basicConfig(level=logging.DEBUG)  # uncomment for debug info
 
 CREDENTIALS = ("3584573970584", "2Vqf6AGTedRERwMVm3WdnU0DCbs=")
 
 if __name__ == "__main__":
 	layers = (
 		KeeperLayer,
-		(YowAuthenticationProtocolLayer, YowMessagesProtocolLayer, YowReceiptProtocolLayer, YowAckProtocolLayer)
-	) + YOWSUP_CORE_LAYERS
+		(  # these protocol layers are parallel layers
+			YowAuthenticationProtocolLayer,
+			YowMessagesProtocolLayer,
+			YowReceiptProtocolLayer,
+			YowAckProtocolLayer,
+			YowMediaProtocolLayer,
+			YowIqProtocolLayer,
+			YowCallsProtocolLayer
+		),
+		YowAxolotlLayer,  # we don't use basic layers because of disconnects, see https://github.com/tgalal/yowsup/issues/873
+		YowLoggerLayer,
+		YowCoderLayer,
+		YowCryptLayer,
+		YowStanzaRegulator,
+		YowNetworkLayer
+	)
 
 	stack = YowStack(layers)
 	stack.setProp(KeeperLayer.KEEPER_NUMBER, "%s@s.whatsapp.net" % CREDENTIALS[0])
