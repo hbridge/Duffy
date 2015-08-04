5e687975d1b56928aaea4526bbac632efa1de68c
Derek Parham <dparham@gmail.com>
2015-08-04 13:48:01 +0000

    Allow ppl to type change and a time

--- peanut/smskeeper/engine/changetime_specific.py
+++ peanut/smskeeper/engine/changetime_specific.py
@@ -10,11 +10,14 @@ logger = logging.getLogger(__name__)
 class ChangetimeSpecificAction(ChangetimeAction):
 	ACTION_CLASS = keeper_constants.CLASS_CHANGETIME_SPECIFIC
 
+	beginsWithRe = r'(change|snooze|update) '
+
 	def getScore(self, chunk, user):
 		score = 0.0
 
 		nattyResult = chunk.getNattyResult(user)
 		bestEntries = self.getEntriesToExecuteOn(chunk, user)
+		okEntries = self.getEntriesToExecuteOn(chunk, user, 65)
 		regexHit = self.snoozeRegex.search(chunk.normalizedText()) is not None
 		justNotified = (user.last_state == "remindersent")
 
@@ -30,18 +33,21 @@ class ChangetimeSpecificAction(ChangetimeAction):
 			else:
 				score = 0.7
 
+		if chunk.matches(self.beginsWithRe) and nattyResult and len(okEntries) > 0:
+			score = 0.95
+
 		if ChangetimeSpecificAction.HasHistoricalMatchForChunk(chunk):
 			score = 1.0
 
 		return score
 
 	# execute is in the parent ChangetimeAction
-	def getEntriesToExecuteOn(self, chunk, user):
+	def getEntriesToExecuteOn(self, chunk, user, score=80):
 		msg = chunk.normalizedTextWithoutTiming(user)
 
 		msg = msg_util.cleanedReminder(msg)
 		msg = msg_util.cleanedSnoozeCommand(msg)
 
-		entries = entry_util.fuzzyMatchEntries(user, chunk.normalizedTextWithoutTiming(user), 80)
+		entries = entry_util.fuzzyMatchEntries(user, chunk.normalizedTextWithoutTiming(user), score)
 		entries = filter(lambda x: not x.hidden, entries)
 		return entries
