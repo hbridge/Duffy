242d91efd140e6e8ab350cb42329ac9008ed6557
asood123 <asood123@yahoo.com>
2015-08-17 22:52:57 +0000

    Revert "Revert "support ctrl-return for creating entries""
    This reverts commit 0fd2bce04117295ffd781a885b079e826afc89f7.

--- peanut/smskeeper/web/src/js/controls/AdminEntriesView.jsx
+++ peanut/smskeeper/web/src/js/controls/AdminEntriesView.jsx
@@ -106,6 +106,17 @@ var CreateEntryInput = React.createClass({
   getInitialState: function(){
     return {sendDisabled: true}
   },
+
+  componentDidMount: function() {
+    $(this.refs.text.getInputDOMNode()).keydown(function (e) {
+        if (e.ctrlKey && (e.keyCode == 13 || e.keyCode == 10)) {
+          if (!this.state.sendDisabled) {
+            this.createEntry(e);
+          }
+      }
+    }.bind(this));
+  },
+
   render: function() {
     var createButton =
       <Button
