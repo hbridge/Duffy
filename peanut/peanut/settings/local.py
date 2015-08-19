b72f76a6dc8dedbedc2b05ae89bdac0ea9d034e5
hbridge <hbridge@gmail.com>
2015-08-19 13:21:05 -0400

    force off texting in local

--- peanut/peanut/settings/local.py
+++ peanut/peanut/settings/local.py
@@ -19,6 +19,8 @@ class CELERY_CONFIG(BASE_CELERY_CONFIG):
 import logging
 logging.getLogger('django.db.backends').setLevel(logging.ERROR)
 
+# force texts to not send
+KEEPER_NUMBER_DICT = {0: KEEPER_NUMBER, 1: KEEPER_NUMBER, 2: KEEPER_NUMBER, 3: KEEPER_NUMBER}
 WHATSAPP_SMS_URL = "http://localhost:7500/smskeeper/incoming_sms"
 WHATSAPP_CREDENTIALS = ("", "")
 
