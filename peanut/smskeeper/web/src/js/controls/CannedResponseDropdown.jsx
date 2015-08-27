d5c968d6249891236ec442f7247a4b90c1cfcdd2
hbridge <hbridge@gmail.com>
2015-08-27 12:26:20 -0400

    add canned responses

--- peanut/smskeeper/web/src/js/controls/CannedResponseDropdown.jsx
+++ peanut/smskeeper/web/src/js/controls/CannedResponseDropdown.jsx
@@ -5,10 +5,12 @@ var Bootstrap = require('react-bootstrap');
 var Utils = require('../utils.js');
 
 var cannedResponses = [
-	"Psst:ear:... for faster responses, give me a day or a time :clock3: with every task that you enter. :sunglasses:",
-	"Psst:ear:... you can tell me \"Done with all\" to check off all tasks. :sunglasses:",
-	"Psst:ear:... you can say \"My zipcode is 12345\" to tell me when you move. :sunglasses:",
-  "I don't know how to do that yet :frowning:. I'll ask my minions :smiley_cat: to teach me."
+	":bulb: Pro tip: for faster responses, give me a day or a time :clock3: when you ask for a reminder. :sunglasses:",
+	":bulb: Pro tip: you can say \"Done with all\" to check off all your tasks. :white_check_mark:",
+	":bulb: Pro tip: you can say \"My zipcode is 12345\" to tell me when you move. :airplane:",
+  ":hatching_chick: I don't know how to do that yet. I'll ask my minions to teach me. :smiley_cat:",
+  "That's not very nice. Keep it up and I'll stop talking to you. :angry:",
+  "Ok, gotta get back to work :information_desk_person: let me know if you need any reminders!",
 ];
 
 module.exports = React.createClass({
