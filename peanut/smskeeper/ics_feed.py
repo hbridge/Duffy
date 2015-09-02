39879c5da5d30c1967ca27b0ec6d97587da1bea5
hbridge <hbridge@gmail.com>
2015-09-02 18:28:28 -0400

    Aggregate cal events at same time into one event

--- peanut/smskeeper/ics_feed.py
+++ peanut/smskeeper/ics_feed.py
@@ -1,5 +1,6 @@
 import json
 import datetime
+import pytz
 import os
 import sys
 import logging
@@ -21,6 +22,8 @@ from icalendar import Calendar
 from icalendar import Event
 from icalendar import vCalAddress
 from icalendar import vText
+from icalendar import vDatetime
+
 
 
 def icsFeed(request, key):
@@ -39,28 +42,67 @@ def icsFeedForUser(user):
 	cal.add('version', '2.0')
 
 	weekAgo = date_util.now() - datetime.timedelta(days=7)
-	entries = Entry.fetchEntries(user, label="#reminders", hidden=None, orderByString="-remind_timestamp")
+	entries = Entry.fetchEntries(user, label="#reminders", hidden=False, orderByString="remind_timestamp")
 	entries = entries.exclude(remind_timestamp__lt=weekAgo)
+
+	entryEvents = []
 	for entry in entries:
+		# if the date is the same, add the entry to the current entry event
+		if len(entryEvents) > 0 and entryEvents[-1].equalToDateTime(entry.remind_timestamp):
+			entryEvents[-1].addEntry(entry)
+		else:
+			entryEvents.append(EntryEvent(entry, user))
+
+	for entryEvent in entryEvents:
+		cal.add_component(entryEvent.asIcsEvent())
+
+	return cal.to_ical()
+
+
+class EntryEvent:
+	entries = []
+	user = None
+
+	def __init__(self, entry, user):
+		self.entries = [entry]
+		self.user = user
+
+	def addEntry(self, entry):
+		if len(self.entries) > 0 and not self.equalToDateTime(entry.remind_timestamp):
+			raise NameError("Tried to add events with conflicting remind_timestamp to EntryEvent. TimeStamps: %s, %s" % (
+				entry.remind_timestamp, self.entries[0].remind_timestamp
+			))
+
+		self.entries.append(entry)
+
+	def equalToDateTime(self, dt):
+		if len(self.entries) == 0:
+			return None
+
+		myVDatetime = vDatetime(self.entries[0].remind_timestamp.replace(second=0)).to_ical()
+		otherVDateTime = vDatetime(dt.replace(second=0)).to_ical()
+		print "myVDatetime: %s otherVDateTime: %s" % (myVDatetime, otherVDateTime)
+		return myVDatetime == otherVDateTime
+
+	def asIcsEvent(self):
 		event = Event()
-		event.add('summary', entry.text)
-		event.add('dtstart', entry.remind_timestamp)
-		event.add('dtend', entry.remind_timestamp + datetime.timedelta(hours=1))
-		event.add('dtstamp', entry.added)
+
+		event.add('summary', ", ".join(map(lambda entry: entry.text, self.entries)))
+
+		event.add('dtstart', self.entries[0].remind_timestamp.replace(second=0))
+		event.add('dtend', self.entries[0].remind_timestamp.replace(second=0) + datetime.timedelta(hours=1))
+		event.add('dtstamp', self.entries[0].added)  # this is not accurate, but shouldn't matter
 
 		# people
 		organizer = vCalAddress('MAILTO:support@getkeeper.com')
 		organizer.params['cn'] = vText('Keeper')
 		organizer.params['role'] = vText('CHAIR')
 		event['organizer'] = organizer
-		event['uid'] = entry.id
+		event['uid'] = "d".join(map(lambda entry: str(entry.id), self.entries))
 		event.add('priority', 5)
 
-		attendee = vCalAddress('SMS:' + user.phone_number)
-		attendee.params['cn'] = vText(user.name)
+		attendee = vCalAddress('SMS:' + self.user.phone_number)
+		attendee.params['cn'] = vText(self.user.name)
 		attendee.params['ROLE'] = vText('REQ-PARTICIPANT')
 		event.add('attendee', attendee, encode=0)
-
-		cal.add_component(event)
-
-	return cal.to_ical()
+		return event
