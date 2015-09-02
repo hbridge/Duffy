02f98ee9efc4fd43fe8db668cab619b8fbf9e479
asood123 <asood123@yahoo.com>
2015-09-02 21:50:27 +0000

    updating regex scheme to prevent random matches

--- peanut/peanut/urls.py
+++ peanut/peanut/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns(
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^memfresh/', include('memfresh.urls')),
 	url(r'^smskeeper/', include('smskeeper.urls')),
-	url(r'^ics/K?P?(?P<key>[A-Za-z0-9]+)', 'smskeeper.ics_feed.icsFeed'),
-	url(r'^K?P?(?P<key>[A-Za-z0-9]+)', 'smskeeper.views.mykeeper'),  # keys can start with either K or P
+	url(r'^ics/[KP](?P<key>[A-Za-z0-9]+)', 'smskeeper.ics_feed.icsFeed'),
+	url(r'^[KP](?P<key>[A-Za-z0-9]+)', 'smskeeper.views.mykeeper'),  # keys can start with either K or P
 	url(r'^ios-notifications/', include('ios_notifications.urls')),
 )
