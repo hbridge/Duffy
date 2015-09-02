0a1521d497b2f64eec292559ed59674af5432afc
hbridge <hbridge@gmail.com>
2015-09-02 16:33:15 -0400

    initial ical feed implementation (requires pip install icalendar)

--- peanut/peanut/urls.py
+++ peanut/peanut/urls.py
@@ -3,13 +3,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^memfresh/', include('memfresh.urls')),
-    url(r'^smskeeper/', include('smskeeper.urls')),
-
-    url(r'^K(?P<key>[A-Za-z0-9]+)', 'smskeeper.views.mykeeper'),
-
-    url(r'^ios-notifications/', include('ios_notifications.urls')),
+urlpatterns = patterns(
+	'',
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^memfresh/', include('memfresh.urls')),
+	url(r'^smskeeper/', include('smskeeper.urls')),
+	url(r'^ics/K(?P<key>[A-Za-z0-9]+)', 'smskeeper.ics_feed.icsFeed'),
+	url(r'^K(?P<key>[A-Za-z0-9]+)', 'smskeeper.views.mykeeper'),
+	url(r'^ios-notifications/', include('ios_notifications.urls')),
 )
---
+++ peanut/smskeeper/ics_feed.py
@@ -0,0 +1,64 @@
+import json
+import datetime
+import os
+import sys
+import logging
+
+parentPath = os.path.join(os.path.split(os.path.abspath(__file__))[0], "..")
+if parentPath not in sys.path:
+	sys.path.insert(0, parentPath)
+import django
+django.setup()
+
+from django.http import HttpResponse
+from smskeeper.models import User
+from smskeeper.models import Entry
+from common import date_util
+
+logger = logging.getLogger(__name__)
+
+from icalendar import Calendar
+from icalendar import Event
+from icalendar import vCalAddress
+from icalendar import vText
+
+def icsFeed(request, key):
+	try:
+		user = User.objects.get(key="K" + key)
+		icsText = icsFeedForUser(user)
+		return HttpResponse(icsText, content_type="text/calendar", status=200)
+	except User.DoesNotExist:
+		return HttpResponse(json.dumps({"Errors": "User not found"}), content_type="text/json", status=400)
+
+
+def icsFeedForUser(user):
+	cal = Calendar()
+	cal.add('prodid', "-//%s's Keeper Calendar//getkeeper.com//" % user.name)
+	cal.add('version', '2.0')
+
+	weekAgo = date_util.now() - datetime.timedelta(days=7)
+	entries = Entry.fetchEntries(user, label="#reminders", hidden=None, orderByString="-remind_timestamp")
+	entries = entries.exclude(remind_timestamp__lt=weekAgo)
+	for entry in entries:
+		event = Event()
+		event.add('summary', entry.text)
+		event.add('dtstart', entry.remind_timestamp)
+		event.add('dtend', entry.remind_timestamp + datetime.timedelta(hours=1))
+		event.add('dtstamp', entry.added)
+
+		# people
+		organizer = vCalAddress('MAILTO:support@getkeeper.com')
+		organizer.params['cn'] = vText('Keeper')
+		organizer.params['role'] = vText('CHAIR')
+		event['organizer'] = organizer
+		event['uid'] = entry.id
+		event.add('priority', 5)
+
+		attendee = vCalAddress('SMS:' + user.phone_number)
+		attendee.params['cn'] = vText(user.name)
+		attendee.params['ROLE'] = vText('REQ-PARTICIPANT')
+		event.add('attendee', attendee, encode=0)
+
+		cal.add_component(event)
+
+	return cal.to_ical()
