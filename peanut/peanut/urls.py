03055beb6b613b21a08e26c1ee80deb4ef09fea4
asood123 <asood123@yahoo.com>
2015-09-02 21:02:26 +0000

    fixing regex for url to support both types of keys'

--- peanut/peanut/urls.py
+++ peanut/peanut/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns(
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^memfresh/', include('memfresh.urls')),
 	url(r'^smskeeper/', include('smskeeper.urls')),
-	url(r'^ics/K(?P<key>[A-Za-z0-9]+)', 'smskeeper.ics_feed.icsFeed'),
-	url(r'^K?(?P<key>[A-Za-z0-9]+)', 'smskeeper.views.mykeeper'),
+	url(r'^ics/K?P?(?P<key>[A-Za-z0-9]+)', 'smskeeper.ics_feed.icsFeed'),
+	url(r'^K?P?(?P<key>[A-Za-z0-9]+)', 'smskeeper.views.mykeeper'),
 	url(r'^ios-notifications/', include('ios_notifications.urls')),
 )
