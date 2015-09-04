b53fd1018bc74b9f4b94857add07e157545e856a
hbridge <hbridge@gmail.com>
2015-09-04 13:49:02 -0400

    merge migrations

---
+++ peanut/smskeeper/migrations/0083_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('smskeeper', '0082_historicaluser'),
+        ('smskeeper', '0081_auto_20150904_1511'),
+    ]
+
+    operations = [
+    ]
