538902f489cea86fd92209434bc9a64340d67584
hbridge <hbridge@gmail.com>
2015-09-09 16:05:33 -0400

    add a config to simulate prod locally

---
+++ peanut/smskeeper/scripts/simulate/local_prod_simulation.py
@@ -0,0 +1,12 @@
+# to run use ./manage.py test smskeeper.scripts.simulateClassifiedMessages
+from smskeeper.scripts.simulate import simulation
+
+
+# Use prod messages, but simulate and upload results locally
+class SMSKeeperLocalSimulationCase(simulation.SMSKeeperSimulationCase):
+	SIMULATION_CONFIGURATION = {
+		'message_source': 'p',  # messages are local
+		'sim_type': 't',  # test
+		'classified_messages_url': "http://prod.strand.duffyapp.com/smskeeper/classified_messages_feed",
+		'post_results_url': "http://localhost:7500/smskeeper/simulation_result/"
+	}
