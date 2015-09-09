e6575bda7f373e3ef0d1345b04b480fcdd81458a
Derek Parham <dparham@gmail.com>
2015-09-09 18:31:30 +0000

    Create new merged migration

---
+++ peanut/smskeeper/migrations/0089_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('smskeeper', '0088_auto_20150909_1756'),
+        ('smskeeper', '0088_auto_20150908_2146'),
+    ]
+
+    operations = [
+    ]
