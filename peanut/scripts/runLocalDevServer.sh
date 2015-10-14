99a15a308104115f33074fa57c8b130d4597d68d
hbridge <hbridge@gmail.com>
2015-10-14 13:33:54 -0400

    make runLocalDevServer run smrt_server

--- peanut/scripts/runLocalDevServer.sh
+++ peanut/scripts/runLocalDevServer.sh
@@ -6,6 +6,12 @@ killall java
 mvn -f ../natty/pom.xml exec:java &
 ./scripts/celery.sh stop local
 ./scripts/celery.sh start local
+launchctl unload scripts/LaunchAgents/com.duffyapp.osx.smrt_server.plist
+launchctl load scripts/LaunchAgents/com.duffyapp.osx.smrt_server.plist
 rm /tmp/mysql.sock
 ln -s /Applications/MAMP/tmp/mysql/mysql.sock /tmp/mysql.sock
 ./manage.py runserver 0.0.0.0:7500
+
+# after ctrl-c this will execute
+echo "\nCleaning up..."
+launchctl unload scripts/LaunchAgents/com.duffyapp.osx.smrt_server.plist
